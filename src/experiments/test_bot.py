33e19ea1b13f090a34fa49d075c49fea26c5be86
zxteloiv <zxteloiv@outlook.com>
2019-06-30 23:16:20 +0800

    test file for the new framework

---
+++ src/experiments/test_bot.py
@@ -0,0 +1,67 @@
+import allennlp.modules
+import config
+import data_adapter.ns_vocabulary
+import training.exp_runner.trial_bot as trial_bot
+
+from models.transformer.parallel_seq2seq import ParallelSeq2Seq
+from models.transformer.encoder import TransformerEncoder
+from models.transformer.decoder import TransformerDecoder
+from allennlp.common.util import START_SYMBOL, END_SYMBOL
+from models.modules.mixture_softmax import MoSProjection
+
+import training.exp_runner as exp_runner
+
+@config.register_hparams
+def weibo_trans_mos():
+    hparams = config.common_settings()
+    hparams.emb_sz = 300
+    hparams.batch_sz = 128
+    hparams.num_layers = 6
+    hparams.num_heads = 8
+    hparams.feedforward_dropout = .2
+    hparams.max_decoding_len = 30
+    hparams.ADAM_LR = 1e-4
+    hparams.TRAINING_LIMIT = 20
+    hparams.mixture_num = 15
+    hparams.beam_size = 1
+    hparams.diversity_factor = 0.
+    hparams.acc_factor = 1.
+    return hparams
+
+def get_model(hparams, vocab: data_adapter.ns_vocabulary.NSVocabulary):
+    encoder = TransformerEncoder(input_dim=hparams.emb_sz,
+                                 num_layers=hparams.num_layers,
+                                 num_heads=hparams.num_heads,
+                                 feedforward_hidden_dim=hparams.emb_sz,)
+    decoder = TransformerDecoder(input_dim=hparams.emb_sz,
+                                 num_layers=hparams.num_layers,
+                                 num_heads=hparams.num_heads,
+                                 feedforward_hidden_dim=hparams.emb_sz,
+                                 feedforward_dropout=hparams.feedforward_dropout,)
+    embedding = allennlp.modules.Embedding(num_embeddings=vocab.get_vocab_size(),
+                                           embedding_dim=hparams.emb_sz)
+    projection_layer = MoSProjection(hparams.mixture_num, decoder.hidden_dim, vocab.get_vocab_size())
+    model = ParallelSeq2Seq(vocab=vocab,
+                            encoder=encoder,
+                            decoder=decoder,
+                            source_embedding=embedding,
+                            target_embedding=embedding,
+                            target_namespace='tokens',
+                            start_symbol=START_SYMBOL,
+                            eos_symbol=END_SYMBOL,
+                            max_decoding_step=hparams.max_decoding_len,
+                            output_projection_layer=projection_layer,
+                            output_is_logit=False,
+                            beam_size=hparams.beam_size,
+                            diversity_factor=hparams.diversity_factor,
+                            accumulation_factor=hparams.acc_factor,
+                            )
+    return model
+
+if __name__ == '__main__':
+    try:
+        bot = trial_bot.TrialBot()
+        bot.run()
+
+    except KeyboardInterrupt:
+        pass
