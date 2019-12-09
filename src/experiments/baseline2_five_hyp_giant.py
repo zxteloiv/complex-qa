a7e009003b72244528b573715be86a0c95910682
zxteloiv <zxteloiv@outlook.com>
2019-12-09 23:03:05 +0800

    run baseline with giant.

---
+++ src/experiments/baseline2_five_hyp_giant.py
@@ -0,0 +1,185 @@
+import sys
+sys.path.insert(0, '..')
+from typing import List, Generator, Tuple, Mapping, Optional
+import logging
+import torch.nn
+import numpy as np
+import random
+
+from trialbot.data import NSVocabulary, PADDING_TOKEN
+from trialbot.training import Registry, TrialBot, Events
+from trialbot.training.updater import TrainingUpdater, TestingUpdater
+from trialbot.training.hparamset import HyperParamSet
+from trialbot.utils.move_to_device import move_to_device
+
+from utils.root_finder import find_root
+_ROOT = find_root()
+
+@Registry.hparamset()
+def atis_five_giant():
+    p = HyperParamSet.common_settings(_ROOT)
+    p.encoder_kernel_size = 3
+    p.num_classes = 2     # either 0 (true) or 1 (false), only 2 classes
+    p.fusion = "full"         # simple, full
+    p.alignment = "linear"    # identity, linear
+    p.connection = "aug"      # none, residual, aug
+    p.prediction = "full"     # simple, full, symmetric
+    p.encoder = "lstm"
+    p.emb_sz = 200
+    p.hidden_size = 100
+    p.num_stacked_block = 2
+    p.num_stacked_encoder = 2
+    p.dropout = .2
+    p.TRAINING_LIMIT = 50
+    p.batch_sz = 32
+    p.use_giant = True
+    return p
+
+@Registry.hparamset()
+def django_fifteen_giant():
+    hparams = atis_five_giant()
+    hparams.TRAINING_LIMIT = 20
+    return hparams
+
+import datasets.atis_rank
+import datasets.atis_rank_translator
+import datasets.django_rank
+import datasets.django_rank_translator
+
+def get_model(hparams, vocab):
+    from experiments.build_model import get_re2_model, get_re2_variant, get_giant_model
+    if hasattr(hparams, "use_giant") and hparams.use_giant == True:
+        return get_giant_model(hparams, vocab)
+    elif hasattr(hparams, "encoder") and hparams.encoder == "lstm":
+        return get_re2_variant(hparams, vocab)
+    else:
+        return get_re2_model(hparams, vocab)
+
+class GiantTrainingUpdater(TrainingUpdater):
+    def update_epoch(self):
+        model, optim, iterator = self._models[0], self._optims[0], self._iterators[0]
+        if iterator.is_new_epoch:
+            self.stop_epoch()
+
+        device = self._device
+        model.train()
+        optim.zero_grad()
+
+        batch = next(iterator)
+        sent_a, sent_b, label = list(map(batch.get, ("source_tokens", "hyp_tokens", "hyp_label")))
+
+        if device >= 0:
+            sent_a = move_to_device(sent_a, device)
+            sent_b = move_to_device(sent_b, device)
+            label = move_to_device(label, device)
+
+        loss = model(sent_a, sent_b, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(model.parameters(), self._grad_clip_val)
+
+        # do some clipping
+        if torch.isnan(loss).any():
+            logging.getLogger().error("NaN loss encountered")
+        else:
+            optim.step()
+        return {"loss": loss}
+
+    @classmethod
+    def from_bot(cls, bot: TrialBot):
+        obj: TrainingUpdater = super().from_bot(bot)
+        del obj._optims
+
+        args, hparams, model = bot.args, bot.hparams, bot.model
+        bot.logger.info("Changed to AdamW with not only the same lr, beta, but also the default AdamW weight decay")
+        optim = torch.optim.AdamW(model.parameters(), hparams.ADAM_LR, hparams.ADAM_BETAS)
+        obj._optims = [optim]
+
+        obj._grad_clip_val = hparams.GRAD_CLIPPING
+        return obj
+
+class GiantTestingUpdater(TestingUpdater):
+    def update_epoch(self):
+        model, iterator, device = self._models[0], self._iterators[0], self._device
+        model.eval()
+        batch = next(iterator)
+        eid, hyp_rank, sent_a, sent_b = list(map(batch.get, ("ex_id", "hyp_rank", "source_tokens", "hyp_tokens")))
+        if iterator.is_new_epoch:
+            self.stop_epoch()
+
+        if device >= 0:
+            sent_a = move_to_device(sent_a, device)
+            sent_b = move_to_device(sent_b, device)
+
+        scores = model.inference(sent_a, sent_b)
+        correct_score = sum(scores) / len(scores)
+        return {"ranking_score": correct_score, "ex_id": eid, "hyp_rank": hyp_rank}
+
+def main():
+    import sys
+    args = sys.argv[1:]
+    if '--dataset' not in sys.argv:
+        args += ['--dataset', 'atis_five_hyp']
+    if '--translator' not in sys.argv:
+        args += ['--translator', 'atis_rank']
+
+    parser = TrialBot.get_default_parser()
+    args = parser.parse_args(args)
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    elif args.quiet:
+        logging.getLogger().setLevel(logging.WARNING)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
+    bot = TrialBot(trial_name="baseline2_giant", get_model_func=get_model, args=args)
+    bot.translator.turn_special_token(on=True)
+    if args.test:
+        import trialbot
+        new_engine = trialbot.training.trial_bot.Engine()
+        new_engine.register_events(*Events)
+        bot._engine = new_engine
+
+        @bot.attach_extension(Events.ITERATION_COMPLETED)
+        def print_output(bot: TrialBot):
+            import json
+            output = bot.state.output
+            if output is None:
+                return
+
+            output_keys = ("ex_id", "hyp_rank", "ranking_score")
+            for eid, hyp_rank, score in zip(*map(output.get, output_keys)):
+                print(json.dumps(dict(zip(output_keys, (eid, hyp_rank, score.item())))))
+
+        bot.updater = GiantTestingUpdater.from_bot(bot)
+    else:
+        from trialbot.training.extensions import every_epoch_model_saver
+
+        @bot.attach_extension(Events.ITERATION_COMPLETED)
+        def end_with_nan_loss(bot: TrialBot):
+            output = bot.state.output
+            if output is None:
+                return
+            loss = output["loss"]
+            def _isnan(x):
+                if isinstance(x, torch.Tensor):
+                    return bool(torch.isnan(x).any())
+                elif isinstance(x, np.ndarray):
+                    return bool(np.isnan(x).any())
+                else:
+                    import math
+                    return math.isnan(x)
+
+            if _isnan(loss):
+                bot.logger.error("NaN loss encountered, training ended")
+                bot.state.epoch = bot.hparams.TRAINING_LIMIT + 1
+                bot.updater.stop_epoch()
+
+
+        bot.add_event_handler(Events.EPOCH_COMPLETED, every_epoch_model_saver, 100)
+        bot.updater = GiantTrainingUpdater.from_bot(bot)
+    bot.run()
+
+if __name__ == '__main__':
+    main()
+
+
