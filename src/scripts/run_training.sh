1f9cf4492dccff3acfc4d7728bf5155fa6b8b8b6
zxteloiv <zxteloiv@outlook.com>
2020-01-16 18:37:49 +0800

    training with the same parameters on halved beam size dataset.

--- src/scripts/run_training.sh
+++ src/scripts/run_training.sh
@@ -1,38 +1,38 @@
 #!/bin/bash 
 # Start training on two models, and two datasets
 
-CUDA_VISIBLE_DEVICES=2 nohup python -u baseline2_five_hyp.py \
+CUDA_VISIBLE_DEVICES=1 nohup python -u baseline2_five_hyp.py \
     -p django_neo_v2 \
     --translator django_rank \
-    --dataset django_thirty_hyp \
+    --dataset django_fifteen_hyp \
     --device 0 \
     --vocab-dump django_vocab_15 \
-    --memo fixed-seed \
-    &> log.django.re2-fixed-seed &
+    --memo re2-base \
+    &> log.django.re2-base &
 
 CUDA_VISIBLE_DEVICES=3 nohup python -u baseline2_five_hyp.py \
     -p atis_neo \
     --translator atis_rank \
-    --dataset atis_ten_hyp \
+    --dataset atis_five_hyp \
     --device 0 \
     --vocab-dump atis_vocab \
     --memo fixed-seed \
-    &> log.atis.re2-fixed-seed &
+    &> log.atis.re2-base &
 
 CUDA_VISIBLE_DEVICES=0 nohup python -u baseline2_five_hyp_giant.py \
     -p django_neo_giant_v2 \
     --translator django_rank \
-    --dataset django_thirty_hyp \
+    --dataset django_fifteen_hyp \
     --device 0 \
     --vocab-dump django_vocab_15 \
-    --memo repaired-giant \
-    &> log.django.giant-repaired &
+    --memo giant-base \
+    &> log.django.giant-base &
 
-CUDA_VISIBLE_DEVICES=1 nohup python -u baseline2_five_hyp_giant.py \
+CUDA_VISIBLE_DEVICES=2 nohup python -u baseline2_five_hyp_giant.py \
     -p atis_neo_giant \
     --translator atis_rank \
-    --dataset atis_ten_hyp \
+    --dataset atis_five_hyp \
     --device 0 \
     --vocab-dump atis_vocab \
-    --memo repaired-giant \
-    &> log.atis.giant-repaired &
+    --memo giant-base \
+    &> log.atis.giant-base &
