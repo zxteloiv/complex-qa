6b79e9597b67e25753f004d90fcfb6a799b67231
zxteloiv <zxteloiv@outlook.com>
2020-05-26 19:56:35 +0800

    use char translator.

--- src/scripts/run_training_giant_five.sh
+++ src/scripts/run_training_giant_five.sh
@@ -3,7 +3,7 @@
 
 CUDA_VISIBLE_DEVICES=2 nohup python -u baseline2_five_hyp_giant.py \
     -p django_deep_giant \
-    --translator django_rank \
+    --translator django_rank_char \
     --dataset django_five_hyp \
     --device 0 \
     --vocab-dump django_vocab_15 \
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=2 nohup python -u baseline2_five_hyp_giant.py \
 
 CUDA_VISIBLE_DEVICES=3 nohup python -u baseline2_five_hyp_giant.py \
     -p atis_deep_giant \
-    --translator atis_rank \
+    --translator atis_rank_char \
     --dataset atis_five_hyp \
     --device 0 \
     --vocab-dump atis_vocab \
