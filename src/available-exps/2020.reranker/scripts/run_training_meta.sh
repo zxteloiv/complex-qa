64bd1dd6a194fac2bc966f88b479b4bfa5429211
zxteloiv <zxteloiv@outlook.com>
2020-08-14 02:22:24 +0800

    metaranker: test with only django and modify the log and snapshot label for lfted.

--- src/available-exps/2020.reranker/scripts/run_training_meta.sh
+++ src/available-exps/2020.reranker/scripts/run_training_meta.sh
@@ -33,43 +33,43 @@ CUDA_VISIBLE_DEVICES=3 nohup python -u meta_ranker.py \
     --dataset django_five_hyp \
     --device 0 \
     --vocab-dump django_vocab_15 \
-    --memo qmaml-lfngram \
-    &> log.django.lfngram.qmaml &
-
-CUDA_VISIBLE_DEVICES=0 nohup python -u meta_ranker.py \
-    -p atis_nl_bert \
-    --translator atis_rank_char \
-    --dataset atis_five_hyp \
-    --device 0 \
-    --vocab-dump atis_vocab \
-    --memo qmaml-nlbert \
-    &> log.atis.nlbert.qmaml &
-
-CUDA_VISIBLE_DEVICES=1 nohup python -u meta_ranker.py \
-    -p atis_nl_ngram \
-    --translator atis_rank_char \
-    --dataset atis_five_hyp \
-    --device 0 \
-    --vocab-dump atis_vocab \
-    --memo qmaml-nlngram \
-    &> log.atis.nlngram.qmaml &
-
-CUDA_VISIBLE_DEVICES=2 nohup python -u meta_ranker.py \
-    -p atis_lf_ngram \
-    --translator atis_rank_char \
-    --dataset atis_five_hyp \
-    --device 0 \
-    --vocab-dump atis_vocab \
-    --memo qmaml-lfngram \
-    &> log.atis.lfngram.qmaml &
-
-CUDA_VISIBLE_DEVICES=3 nohup python -u meta_ranker.py \
-    -p atis_lf_ted \
-    --translator atis_rank_char \
-    --dataset atis_five_hyp \
-    --device 0 \
-    --vocab-dump atis_vocab \
     --memo qmaml-lfted \
-    &> log.atis.lfted.qmaml &
+    &> log.django.lfted.qmaml &
+
+#CUDA_VISIBLE_DEVICES=0 nohup python -u meta_ranker.py \
+#    -p atis_nl_bert \
+#    --translator atis_rank_char \
+#    --dataset atis_five_hyp \
+#    --device 0 \
+#    --vocab-dump atis_vocab \
+#    --memo qmaml-nlbert \
+#    &> log.atis.nlbert.qmaml &
+#
+#CUDA_VISIBLE_DEVICES=1 nohup python -u meta_ranker.py \
+#    -p atis_nl_ngram \
+#    --translator atis_rank_char \
+#    --dataset atis_five_hyp \
+#    --device 0 \
+#    --vocab-dump atis_vocab \
+#    --memo qmaml-nlngram \
+#    &> log.atis.nlngram.qmaml &
+#
+#CUDA_VISIBLE_DEVICES=2 nohup python -u meta_ranker.py \
+#    -p atis_lf_ngram \
+#    --translator atis_rank_char \
+#    --dataset atis_five_hyp \
+#    --device 0 \
+#    --vocab-dump atis_vocab \
+#    --memo qmaml-lfngram \
+#    &> log.atis.lfngram.qmaml &
+#
+#CUDA_VISIBLE_DEVICES=3 nohup python -u meta_ranker.py \
+#    -p atis_lf_ted \
+#    --translator atis_rank_char \
+#    --dataset atis_five_hyp \
+#    --device 0 \
+#    --vocab-dump atis_vocab \
+#    --memo qmaml-lfted \
+#    &> log.atis.lfted.qmaml &
 
 
