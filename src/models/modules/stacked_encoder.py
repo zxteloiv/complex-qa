2a4f0b3e3a7db550abf95517ad57501c7f7631a8
zxteloiv <zxteloiv@outlook.com>
2020-12-17 21:02:53 +0800

    reranker: runnable s2s parser for top dataset; fixed the encoder factory method; fixed default value for the decoder history attention.

--- src/models/modules/stacked_encoder.py
+++ src/models/modules/stacked_encoder.py
@@ -12,7 +12,7 @@ class StackedEncoder(torch.nn.Module):
 
         self.layer_encs = torch.nn.ModuleList(encs)
         self.input_size = input_size or encs[0].get_input_dim()
-        self.output_size = output_size or encs[-1].get_output_dim()
+        self.output_size = output_size
         self.input_dropout = torch.nn.Dropout(input_dropout)
         self.output_every_layer = output_every_layer
 
@@ -43,7 +43,11 @@ class StackedEncoder(torch.nn.Module):
         return self.input_size
 
     def get_output_dim(self):
-        return self.output_size * 2 if self.is_bidirectional() else self.output_size
+        if self.output_size is not None:
+            out_dim = self.output_size * 2 if self.is_bidirectional() else self.output_size
+        else:
+            out_dim = self.layer_encs[-1].get_output_dim()
+        return out_dim
 
     def is_bidirectional(self) -> bool:
         return self.layer_encs[-1].is_bidirectional()
@@ -53,15 +57,17 @@ class StackedEncoder(torch.nn.Module):
         from models.transformer.encoder import TransformerEncoder
         from allennlp.modules.seq2seq_encoders import LstmSeq2SeqEncoder
 
-        inp_sz_fn = lambda floor: p.emb_sz if floor == 0 else p.hidden_sz
         if p.encoder == "lstm":
-            enc_cls = lambda floor: LstmSeq2SeqEncoder(inp_sz_fn(floor), p.hidden_sz, bidirectional=False)
+            enc_cls = lambda floor: LstmSeq2SeqEncoder(p.emb_sz if floor == 0 else p.hidden_sz,
+                                                       p.hidden_sz, bidirectional=False)
         elif p.encoder == "transformer":
             enc_cls = lambda floor: TransformerEncoder(
-                inp_sz_fn(floor), p.hidden_sz, 1, p.num_heads, p.hidden_sz, p.dropout, p.dropout, 0., (floor == 0),
+                p.emb_sz if floor == 0 else p.hidden_sz,
+                p.hidden_sz, 1, p.num_heads, p.hidden_sz, p.dropout, p.dropout, 0., (floor == 0),
             )
         elif p.encoder == "bilstm":
-            enc_cls = lambda floor: LstmSeq2SeqEncoder(inp_sz_fn(floor), p.hidden_sz, bidirectional=True)
+            enc_cls = lambda floor: LstmSeq2SeqEncoder(p.emb_sz if floor == 0 else p.hidden_sz * 2, # bidirectional
+                                                       p.hidden_sz, bidirectional=True)
         else:
             raise NotImplementedError
 
