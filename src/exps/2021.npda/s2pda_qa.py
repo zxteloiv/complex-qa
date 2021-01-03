npda: fixed loss computation, normalized by seq length and derivation length.

src/exps/2021.npda/s2pda_qa.py
@@ -100,9 +100,8 @@ def get_model(p, vocab: NSVocabulary):
         ),
         stack=TensorBatchStack(p.batch_sz, p.max_derivation_step, item_size=1, dtype=torch.long),
         symbol_predictor=symbol_predictor,
-        exact_form_predictor=nn.Sequential(
-
-            MoSProjection(p.num_exact_token_mixture, p.hidden_sz + p.emb_sz, vocab.get_vocab_size(ns_et), output_semantics="probs")
+        exact_form_predictor=MoSProjection(
+            p.num_exact_token_mixture, p.hidden_sz + p.emb_sz, vocab.get_vocab_size(ns_et), output_semantics="probs"
         ),
         query_attention_composer=ClassicMLPComposer(encoder.get_output_dim(), p.hidden_sz, p.hidden_sz),
         grammar_entry=vocab.get_token_index(p.grammar_entry, ns_s),
@@ -171,10 +170,12 @@ def main():
         from trialbot.training.extensions import every_epoch_model_saver
         from utils.trial_bot_extensions import end_with_nan_loss
         from utils.trial_bot_extensions import evaluation_on_dev_every_epoch
+        from utils.trial_bot_extensions import save_model_every_num_iters
         bot.add_event_handler(Events.EPOCH_COMPLETED, evaluation_on_dev_every_epoch, 90)
 
         bot.add_event_handler(Events.ITERATION_COMPLETED, end_with_nan_loss, 100)
         bot.add_event_handler(Events.EPOCH_COMPLETED, every_epoch_model_saver, 100)
+        bot.add_event_handler(Events.ITERATION_COMPLETED, save_model_every_num_iters, 100, interval=100)
 
         # from utils.trial_bot_extensions import init_tensorboard_writer
         # from utils.trial_bot_extensions import write_batch_info_to_tensorboard

src/models/neural_pda/npda.py
@@ -129,7 +129,7 @@ class NeuralPDA(nn.Module):
             # symbol_opts, opt_mask: (batch, opts)
             # compliant_logits: (batch, V)
             symbol_opts, opt_mask = grammar_guide[:, :, 0, step], grammar_guide[:, :, -1, step]
-            compliant_logits = self._learn_step_symbol_from_tutor(symbol_logits, symbol_opts, opt_mask)
+            compliant_logits = self._learn_from_step_grammar_tutor(symbol_logits, symbol_opts, opt_mask)
             step_inp = compliant_logits.argmax(dim=-1)
             mem(symbol_logits=compliant_logits)
 
@@ -142,8 +142,8 @@ class NeuralPDA(nn.Module):
         seq_opts, opt_mask = grammar_guide[:, :, 0, :], grammar_guide[:, :, -1, :]
         seq_opts_logp = torch.gather(seq_logp, index=seq_opts, dim=1)
 
-        # opts_logp: (batch, opt_num)
-        opts_logp = (seq_opts_logp * opt_mask).sum(dim=-1)
+        # opts_logp: (batch, opt_num), the length of optional derivations should be taken out by computing the mean
+        opts_logp = (seq_opts_logp * opt_mask).sum(dim=-1) # / (opt_mask.sum(dim=-1) + 1)
         return opts_logp
 
     def _predict_tree_symbol(self, last_symbol, tree_state) -> Tuple[FT, FT]:
@@ -173,7 +173,7 @@ class NeuralPDA(nn.Module):
             self._tree_state = self._tree_state + rule
 
     @staticmethod
-    def _learn_step_symbol_from_tutor(logit, compliance, compliance_mask) -> FT:
+    def _learn_from_step_grammar_tutor(logit, compliance, compliance_mask) -> FT:
         """
         Adjust the symbol logits at current step.
         Grammar Guide Mask is not required yet, but it will be used in the entire sequence.
@@ -184,6 +184,7 @@ class NeuralPDA(nn.Module):
         """
         # the default weight is 0; set all weights for the positions contained in opts to 1;
         # reset the weights of 0 index with valid 0s
+        compliance = compliance * compliance_mask
         weights = torch.zeros_like(logit).bool()
         weights[torch.arange(logit.size()[0], device=logit.device).unsqueeze(-1), compliance] = 1
         weights[:, 0] = (((compliance == 0) * compliance_mask).sum(-1) > 0) # the mask is ignored at the current step

src/models/neural_pda/seq2pda.py
@@ -73,7 +73,8 @@ class Seq2PDA(nn.Module):
         enc_attn_fn = self._encode_source(source_tokens)
         self.npda.init_automata(source_tokens.size()[0], source_tokens.device, enc_attn_fn)
 
-        step = loss = 0
+        step = 0
+        loss = valid_derivation_num = 0
         exact_token_list = []
         while self.npda.continue_derivation() and step * self.max_expansion_len < mask.size()[1]:
             derivation = self.npda()
@@ -84,6 +85,8 @@ class Seq2PDA(nn.Module):
             loss = loss + derivation_loss.detach()
             step += 1
 
+            # any valid derivation must expand the RHS starting with a START token, and the mask is set to 1.
+            valid_derivation_num += gold[-1][:, 0]
             exact_token_list.append(self._arrange_target_tokens(derivation))
             self.npda.push_predictions_onto_stack(gold[:3], derivation[1])
 
@@ -94,8 +97,9 @@ class Seq2PDA(nn.Module):
         else:
             self.logger.warning('No exact token generated, which is impossible.')
 
-        self.ppl(loss)
-        output = {'loss': loss}
+        normalized_loss = loss / valid_derivation_num.float().mean()
+        self.ppl(normalized_loss)
+        output = {'loss': normalized_loss}
         self.npda.reset_automata()
         return output
 
@@ -132,9 +136,9 @@ class Seq2PDA(nn.Module):
         lhs, lhs_mask, grammar_guide, opts_logp, topo_preds, exact_token_p = derivation
         comp_len = grammar_guide.size()[-1]
 
-        symbol, p_growth, _, exact_token, mask = map(lambda t: t[:, :comp_len], gold)
+        symbol, p_growth, _, exact_token, seq_mask = map(lambda t: t[:, :comp_len], gold)
 
-        et_mask = mask * (p_growth == 0)
+        et_mask = seq_mask * (p_growth == 0)
         et_logp = (exact_token_p + 1e-13).log()
         et_loss = -et_logp.gather(dim=-1, index=exact_token.unsqueeze(-1)).squeeze(-1) * et_mask
         per_batch_loss = et_loss.sum(1) / (et_mask.sum(1) + 1e-13)
@@ -144,15 +148,18 @@ class Seq2PDA(nn.Module):
         # seq_symbol_opts: (batch, opt_num, comp_len)
         seq_symbol_opts = grammar_guide[:, :, 0, :]
 
-        # opt_weights: (batch, opt_num) <- (batch, opt_num, comp_len) <- (batch, 1, comp_len) <- symbol: (batch, comp_len)
         # seq_opt_weights: (batch, opt_num, comp_len)
-        seq_opt_weights = (symbol.unsqueeze(1) == seq_symbol_opts)  # target seq is the same with some derivation
-        seq_opt_weights = seq_opt_weights * mask.unsqueeze(1)   # the comparison is conducted only at valid locations
+        seq_opt_weights = (symbol.unsqueeze(1) == seq_symbol_opts)   # target seq is different with the option
+        seq_opt_weights = seq_opt_weights * seq_mask.unsqueeze(1)    # conducting comparisons only at valid locations
 
         # opt_weights: (batch, opt_num), only the correct opt is marked as 1
-        opt_weights = seq_opt_weights.sum(-1) > 0
+        opt_weights = seq_opt_weights.sum(-1) == seq_mask.sum(dim=-1).unsqueeze(1)
+        batch_mask = seq_mask.sum(dim=-1) > 0
+        topology_loss_per_batch = -(opt_weights * opts_logp).sum(-1) * batch_mask
 
-        topology_loss = -(opt_weights * opts_logp).sum(-1).mean()
+        # any valid derivation has a starting symbol by default and thus must be a non-empty sequence
+        topology_loss = topology_loss_per_batch.sum() / num_non_empty_sequences
+
         return token_loss + topology_loss
 
     def _arrange_target_tokens(self, derivation):