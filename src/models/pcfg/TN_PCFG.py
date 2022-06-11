957cf60302befb76c7e51486299fe7b0afdef658
zxteloiv <zxteloiv@outlook.com>
2022-06-11 14:49:48 +0800

    pcfg: revert to original implementation for TD-PCFG;

--- src/models/pcfg/TN_PCFG.py
+++ src/models/pcfg/TN_PCFG.py
@@ -37,19 +37,23 @@ class TNPCFG(PCFGModule):
         self.term_emb = nn.Parameter(torch.randn(self.T, self.hidden_sz))
         self.nonterm_emb = nn.Parameter(torch.randn(self.NT, self.hidden_sz))
         self.root_emb = nn.Parameter(torch.randn(1, self.hidden_sz))
-        self.word_emb = nn.Parameter(torch.randn(self.V, self.hidden_sz))
 
         self.root_mlp = nn.Sequential(nn.Linear(self.hidden_sz + self.z_dim, self.hidden_sz),
                                       ResLayer(self.hidden_sz, self.hidden_sz),
                                       ResLayer(self.hidden_sz, self.hidden_sz),
                                       nn.Linear(self.hidden_sz, self.NT))
 
-        self.parent_mlp, self.left_mlp, self.right_mlp, self.term_mlp, self.word_mlp = (
+        self.term_mlp = nn.Sequential(nn.Linear(self.hidden_sz + self.z_dim, self.hidden_sz),
+                                      ResLayer(self.hidden_sz, self.hidden_sz),
+                                      ResLayer(self.hidden_sz, self.hidden_sz),
+                                      nn.Linear(self.hidden_sz, self.V))
+
+        self.parent_mlp, self.left_mlp, self.right_mlp = (
             nn.Sequential(nn.Linear(self.hidden_sz + self.z_dim, self.hidden_sz),
                           ResLayer(self.hidden_sz, self.hidden_sz),
                           ResLayer(self.hidden_sz, self.hidden_sz),
                           nn.Linear(self.hidden_sz, self.r))
-            for _ in range(5)
+            for _ in range(3)
         )
 
         if encoder_input_dim is not None:   # otherwise no encoding is required, only for generation
@@ -75,24 +79,19 @@ class TNPCFG(PCFGModule):
     def get_pcfg_params(self, z):
         b = z.size()[0]
 
+        # -----------------------
         # roots: (b, NT)
         root_emb = self.root_emb.expand(b, self.hidden_sz)
         root_emb = torch.cat([root_emb, z], -1)
         roots = self.root_mlp(root_emb).log_softmax(-1)
 
         # -----------------------
-        # decomposed unary rules: P -> x, different from the original TD-PCFG paper
-        # term: (b, T, r)
+        # unary rules: P -> x
+        # term: (b, T, V)
         z_term_ex = z.unsqueeze(1).expand(b, self.T, self.z_dim)
         term_emb = self.term_emb.unsqueeze(0).expand(b, self.T, self.hidden_sz)
         term_cat = torch.cat([term_emb, z_term_ex], -1)
-        term = self.term_mlp(term_cat).log_softmax(-1)
-
-        # word: (b, V, r)
-        z_vocab_ex = z.unsqueeze(1).expand(b, self.V, self.z_dim)
-        word_emb = self.word_emb.unsqueeze(0).expand(b, self.V, self.hidden_sz)
-        word_cat = torch.cat([word_emb, z_vocab_ex], -1)
-        word = self.word_mlp(word_cat).log_softmax(-2)
+        terms = self.term_mlp(term_cat).log_softmax(-1)
 
         # -----------------------
         # decomposed binary rules: A -> B C
@@ -108,7 +107,7 @@ class TNPCFG(PCFGModule):
         left = self.left_mlp(rule_cat).log_softmax(-2)
         right = self.right_mlp(rule_cat).log_softmax(-2)
 
-        return roots, (term, word), (head, left, right)
+        return roots, terms, (head, left, right)
 
     def inside(self, x, pcfg_params, x_hid=None):
         # x, x_mask: (batch, n)
@@ -126,7 +125,7 @@ class TNPCFG(PCFGModule):
         # word: (batch, V, r), normalized on dim=-2
         # head: (batch, NT, r), normalized on dim=-1
         # left/right: (batch, NT_T, r), normalized on dim=-2
-        roots, (term, word), (head, left, right) = pcfg_params
+        roots, terms, (head, left, right) = pcfg_params
         b, n = x.size()
         NTs = slice(0, self.NT)                         # NTs proceed all Ts
 
@@ -155,87 +154,85 @@ class TNPCFG(PCFGModule):
     def _score_chart_init(self, x, pcfg_params):
         # x, x_mask: (batch, n)
         # roots: (batch, NT)
-        # term: (batch, T, r)
-        # word: (batch, V, r)
+        # terms: (batch, T, V)
         # head: (batch, NT, r), normalized on dim=-1
         # left/right: (batch, NT_T, r), normalized on dim=-2
-        roots, (term, word), (head, left, right) = pcfg_params
+        roots, terms, (head, left, right) = pcfg_params
         b, n = x.size()
         s_chart = x.new_full((b, n, n, self.NT_T), -1e12)
 
-        # term_logp: (b, T, V, ~~r~~)
-        term_logp = (_un(term, 2) + _un(word, 1)).logsumexp(-1)
-        # 0, (batch, n, T, 1)
-        level0 = _un(term_logp, 1).expand(b, n, self.T, self.V).gather(
+        level0 = _un(terms, 1).expand(b, n, self.T, self.V).gather(
             index=_un(_un(x, -1).expand(b, n, self.T), -1), dim=-1,
         )
-        s_chart[:, 0, :, self.NT:self.NT_T] = level0.squeeze(-1)
+        s_chart[:, 0, :, self.NT:] = level0.squeeze(-1)
         return s_chart
 
     def _inside_for_encoder(self, x_hid, x_mask, pcfg_params, s_chart):
-        roots, (term, word), (head, left, right) = pcfg_params
+        roots, terms, (head, left, right) = pcfg_params
         b, n = x_hid.size()[:2]
+        a_hid = _un(self.binary_head(self.nonterm_emb), [0, 1])  # (1, 1, A, hid)
         emb_chart = self._emb_chart_init(x_hid, pcfg_params)     # (b, n, n, r, hid) or None
         for width in range(1, n):
             coordinates = self.get_inside_coordinates(n, width, x_hid.device)
             # b/c_score: (batch, pos=(n - width), arrangement=width, NT_T)
             b_score, c_score = self.inside_chart_select(s_chart, coordinates)
 
-            # left/right -> (b, 1, 1, NT_T, r)
-            # b/c_score -> (b, pos, arr, NT_T, 1)
-            # vb/wc_score: (b, pos, arr, ~~NT_T~~, r)
-            vb_score = (_un(left, [1, 2]) + _un(b_score, -1)).logsumexp(dim=-2)
-            wc_score = (_un(right, [1, 2]) + _un(c_score, -1)).logsumexp(dim=-2)
+            # left/right -> (b, 1, 1, B/C=NT_T, r)
+            # b/c_score -> (b, pos, arr, B/C, 1)
+            # vb/wc_score: (b, pos, arr, B/C, r)
+            vb_score = (_un(left, [1, 2]) + _un(b_score, -1)).exp()
+            wc_score = (_un(right, [1, 2]) + _un(c_score, -1)).exp()
 
-            vbwc_score = vb_score + wc_score    # (b, pos, arr, r)
-            layer = self._get_emb_chart_layer(emb_chart, coordinates, pcfg_params, vbwc_score=vbwc_score)
-            emb_chart[:, width, :n - width] = layer
+            # b/c_hid: (b, pos, arr, B/C, hid)
+            b_repr, c_repr = self.inside_chart_select(emb_chart, coordinates)
+            b_hid = self.binary_left(b_repr)
+            c_hid = self.binary_right(c_repr)
+
+            # b_rank: (b, pos, arr, ~~B~~, r, hid)
+            # b_weight: (b, pos, arr, ~~C~~, r, 1)
+            b_rank = (_un(b_hid, -2) + _un(vb_score, -1)).sum(dim=3)
+            b_weight = wc_score.sum(dim=-2).unsqueeze(-1)
+            # b_factor: (b, pos, ~~arr~~, r, hid)
+            b_factor = (b_weight * b_rank).sum(dim=2)
+
+            # (b, pos, arr, ~~C~~, r, hid) <-- (b, pos, arr, C, 1, hid) + (b, pos, arr, C, r, 1)
+            c_rank = (_un(c_hid, -2) + _un(wc_score, -1)).sum(dim=3)
+            # c_weight: (b, pos, arr, ~~B~~, r, 1)
+            c_weight = vb_score.sum(dim=-2).unsqueeze(-1)
+            # c_factor: (b, pos, ~~arr~~, r, hid)
+            c_factor = (c_weight * c_rank).sum(dim=2)
+
+            # (b, pos, A, ~~r~~, hid)
+            bc_factor = (_un(b_factor + c_factor, 2) * _un(head.exp(), [1, 4])).sum(dim=-2)
+
+            a_score = s_chart[:, width, :n - width, :self.NT].unsqueeze(-1)     # (b, pos, A, 1)
+
+            layer = bc_factor / (a_score.exp() + 1e-31) + a_hid
+            emb_chart[:, width, :n - width, :self.NT] = unit_norm(layer)
 
         return emb_chart
 
     def _emb_chart_init(self, x_hid, pcfg_params):
         b, n = x_hid.size()[:2]
-        emb_chart = x_hid.new_zeros(b, n, n, self.r, self.emb_chart_dim)
-
-        roots, (term, word), (head, left, right) = pcfg_params
+        emb_chart = x_hid.new_zeros(b, n, n, self.NT_T, self.emb_chart_dim)
 
         # x_hid: (batch, n, hid)
-        x_unary = self.unary_word(x_hid).unsqueeze(2)       # (b, n, 1, hid)
-
-        t_unary = _un(self.unary_term(self.term_emb), 0)    # (1, T, hid)
-        term_t = term.transpose(1, 2).exp()                 # (b, r, T)
-        r_unary = _un(torch.matmul(term_t, t_unary), 1)     # (b, 1, r, hid)
-
-        emb_chart[:, 0] = x_unary + r_unary                 # (b, n, r, hid)
+        x_unary = _un(self.unary_word(x_hid), 2)                 # (b, n, 1, hid)
+        t_unary = _un(self.unary_term(self.term_emb), [0, 1])    # (1, 1, T, hid)
+        emb_chart[:, 0, :, self.NT:] = unit_norm(x_unary + t_unary)     # (b, n, T, hid)
         return emb_chart
-
-    def _get_emb_chart_layer(self, emb_chart, coordinates, pcfg_params, **ctx):
-        # to save storage, the computation is factored into 3 parts
-        # recall score_arr: (batch, pos, A=NT, arrangement, B, C)
-        vbwc_score = ctx['vbwc_score']      # (b, pos, arr, r)
-        vbwc = _un(vbwc_score, -1).exp()    # (b, pos, arr, r, 1)
-
-        # bc_hid: (b, pos, arr, r, hid)
-        b_repr, c_repr = self.inside_chart_select(emb_chart, coordinates)
-        bc_hid = self.binary_left(b_repr) + self.binary_right(c_repr)
-
-        layer = (bc_hid * vbwc).sum(2)  # (b, pos, ~~arr~~, r, hid)
-        return layer    # (b, pos, r, hid)
 
     def _get_final_attention_memory(self, emb_chart, s_chart, pcfg_params):
         b, n = emb_chart.size()[:2]
-        roots, (term, word), (head, left, right) = pcfg_params
+        # roots: (b, A)
+        roots, terms, (head, left, right) = pcfg_params
 
-        chart = emb_chart[:, 1:]  # (b, n - 1, n, r, hid)
-        chart_ex = chart.reshape(b, (n - 1) * n, self.r, self.emb_chart_dim)
-        r_dist = (head + _un(roots, -1)).logsumexp(dim=-2)  # (b, r)
-        mem_bc = (_un(r_dist, [1, 3]).exp() * chart_ex).sum(dim=2)  # (b, n(n-1), ~~r~~, hid)
+        # (b, n-1, n, A, hid)
+        chart = emb_chart[:, 1:, :, :self.NT]
+        # (b, n(n-1), A, hid)
+        mem_a = chart.reshape(b, (n - 1) * n, self.NT, self.emb_chart_dim)
 
-        a_hid = _un(self.binary_head(self.nonterm_emb), [0, 1])  # (1, 1, NT, hid)
-        a_score = s_chart[:, 1:, :, :self.NT].reshape(b, (n - 1) * n, self.NT, 1)
-        # (b, n(n-1), ~~NT~~, hid)
-        a_factor = ((a_hid * a_score.exp()) * _un(roots.exp(), [1, 3])).sum(dim=2)
-        mem = mem_bc + a_factor
+        mem = (_un(roots, [1, 3]).exp() * mem_a).sum(dim=2)
         return mem
 
     def _generate_next_nonterms(self,
