78a827a7b4bc05568f970255cc6f564cdd82e457
zxteloiv <zxteloiv@outlook.com>
2022-11-10 13:59:13 +0800

    hungarian-s2s: remove libs2s file; add pmi mappings;

---
+++ src/exps/2022.learning-mapping/mappings.py
@@ -0,0 +1,74 @@
+import sys
+from collections import defaultdict
+from itertools import product
+from typing import Sequence, Generator
+
+import tqdm
+from trialbot.utils.root_finder import find_root
+
+sys.path.insert(0, find_root('.SRC'))
+
+
+def iterngrams(s: Sequence, n: int, start=None, end=None) -> Generator[Sequence, None, None]:
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError(f'n-gram requires a positive n, but got {n}')
+
+    start = start or 0
+    end = end or len(s)
+    for i in range(start, end):
+        if i + n <= end:
+            yield s[i:i+n]
+        else:
+            break
+
+
+def get_all_ngrams(s: Sequence, n_min=1, n_max=None):
+    n_max = n_max or len(s)
+    for n in range(n_min, n_max + 1):
+        yield from iterngrams(s, n)
+
+
+def main(args):
+    from datasets.comp_gen_bundle import install_raw_qa_datasets
+    from trialbot.training import Registry
+    install_raw_qa_datasets(Registry._datasets)
+    train, dev, test = Registry.get_dataset(args.dataset)
+    print(len(train), len(dev), len(test))
+
+    nl_max, lf_max = 1, 1
+    pmi, pair_freq, nl_freq, lf_freq = get_pmi_for_dataset(train, nl_max, lf_max)
+    output_file = open(f'pmi-nl{nl_max}-lf{lf_max}.txt', 'w')
+    for (nl, lf), v in sorted(pmi.items(), key=lambda t: t[1], reverse=True):
+        output_file.write(f"{v}\t{nl_freq[nl]}\t{lf_freq[lf]}\t{nl}\t{lf}\n")
+    output_file.close()
+
+
+def get_pmi_for_dataset(dataset, nl_max, lf_max):
+    pair_freq = defaultdict(lambda: 0)
+    nl_freq = defaultdict(lambda: 0)
+    lf_freq = defaultdict(lambda: 0)
+
+    for x in tqdm.tqdm(dataset):
+        sent, sql = x['sent'], x['sql']
+        nl_ngrams = [tuple(ngram) for ngram in get_all_ngrams(sent.split(), n_max=nl_max)] + [()]
+        lf_ngrams = [tuple(ngram) for ngram in get_all_ngrams(sql.split(), n_max=lf_max)] + [()]
+        for ngram in nl_ngrams:
+            nl_freq[ngram] += 1
+        for ngram in lf_ngrams:
+            lf_freq[ngram] += 1
+        for nl_ngram, lf_ngram in product(nl_ngrams, lf_ngrams):
+            pair_freq[nl_ngram, lf_ngram] += 1
+
+    pmi = defaultdict(lambda: 0.)
+    for nl, lf in tqdm.tqdm(product(nl_freq.keys(), lf_freq.keys()), total=len(nl_freq) * len(lf_freq)):
+        if (nl, lf) in pair_freq:
+            pmi[nl, lf] = pair_freq[nl, lf] / nl_freq[nl] / lf_freq[lf]
+
+    return pmi, pair_freq, nl_freq, lf_freq
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', '-d', default='raw_qa.scholar_cg')
+    main(parser.parse_args())
--- src/exps/2022.learning-mapping/s2s.py
+++ src/exps/2022.learning-mapping/s2s.py
@@ -1,9 +1,9 @@
-from trialbot.training import Registry
+from trialbot.training import Registry, TrialBot, Events
+import argparse
 
 
 def main():
     from utils.trialbot.setup_cli import setup as setup_cli
-    from libs2s import setup_common_bot
     import datasets.comp_gen_bundle as cg_bundle
     cg_bundle.install_parsed_qa_datasets(Registry._datasets)
     import datasets.cg_bundle_translator
@@ -13,7 +13,6 @@ def main():
 
 @Registry.hparamset()
 def s2s():
-    from libs2s import base_hparams
     p = base_hparams()
     p.TRAINING_LIMIT = 400
     p.batch_sz = 16
@@ -35,6 +34,123 @@ def hungarian_reg():
 def hungarian_sup():
     p = s2s()
     p.attn_supervision = 'hungarian_sup'
+    return p
+
+
+def get_updater(bot: TrialBot):
+    args, p, model, logger = bot.args, bot.hparams, bot.model, bot.logger
+    from utils.select_optim import select_optim
+
+    if p.encoder.startswith('plm:'):
+        bert_params, nonbert_params = [], []
+        for name, param in model.named_parameters():
+            if 'pretrained_model' in name:  # the attribute is used by SeqPLMEmbedEncoder wrapper.
+                bert_params.append(param)
+            else:
+                nonbert_params.append(param)
+
+        params = [{'lr': 1e-5, 'params': bert_params}, {'params': nonbert_params}]
+    else:
+        params = model.parameters()
+
+    optim = select_optim(p, params)
+    logger.info(f"Using Optimizer {optim}")
+
+    from trialbot.data.iterators import RandomIterator
+    iterator = RandomIterator(len(bot.train_set), p.batch_sz)
+    logger.info(f"Using RandomIterator: on volume={len(bot.train_set)} with batch={p.batch_sz}")
+
+    lr_scheduler_kwargs = getattr(p, 'lr_scheduler_kwargs', None)
+    if lr_scheduler_kwargs is not None:
+        from allennlp.training.learning_rate_schedulers import NoamLR
+        lr_scheduler = NoamLR(optimizer=optim, **lr_scheduler_kwargs)
+        bot.scheduler = lr_scheduler
+        logger.info(f'the scheduler enabled: {lr_scheduler}')
+
+        def _sched_step(bot: TrialBot):
+            bot.scheduler.step_batch()
+            if bot.state.iteration % 100 == 0:
+                logger.info(f"update lr to {bot.scheduler.get_values()}")
+
+        bot.add_event_handler(Events.ITERATION_COMPLETED, _sched_step, 100)
+
+    from trialbot.training.updaters.training_updater import TrainingUpdater
+    updater = TrainingUpdater(bot.train_set, bot.translator, model, iterator, optim, args.device, args.dry_run)
+    return updater
+
+
+def setup_common_bot(args: argparse.Namespace, get_model_func=None, trialname: str = None):
+    assert args is not None
+    from models.base_s2s.model_factory import Seq2SeqBuilder
+    get_model_func = get_model_func or Seq2SeqBuilder.from_param_and_vocab
+    trialname = trialname or f'enc2dec-{args.hparamset}'
+    bot = TrialBot(trial_name=trialname, get_model_func=get_model_func, args=args)
+    from utils.trialbot.setup_bot import setup_bot, add_metric_printing
+
+    if not args.test:
+        bot = setup_bot(bot, epoch_model_saving=False)
+        from trialbot.training.extensions import every_epoch_model_saver
+        if args.seed == 2021 or args.seed == '2021':
+            bot.add_event_handler(Events.EPOCH_COMPLETED, every_epoch_model_saver, 100)
+        bot.updater = get_updater(bot)
+    else:
+        bot = add_metric_printing(bot, 'Testing Metrics')
+
+    return bot
+
+
+def base_hparams():
+    from trialbot.training.hparamset import HyperParamSet
+    from trialbot.utils.root_finder import find_root
+    p = HyperParamSet.common_settings(find_root())
+    p.TRAINING_LIMIT = 150
+    p.WEIGHT_DECAY = 0.
+    p.OPTIM = "adabelief"
+    p.ADAM_BETAS = (0.9, 0.999)
+    p.batch_sz = 16
+
+    p.lr_scheduler_kwargs = {'model_size': 400, 'warmup_steps': 50}
+    p.src_emb_pretrained_file = "~/.glove/glove.6B.100d.txt.gz"
+
+    p.hidden_sz = 300
+    p.dropout = .5
+    p.decoder = "lstm"
+    p.max_decoding_step = 100
+    p.scheduled_sampling = .1
+
+    p.num_enc_layers = 1
+    p.num_dec_layers = 1
+
+    p.tied_decoder_embedding = False
+    p.emb_sz = 100
+
+    p.enc_out_dim = p.hidden_sz
+    p.dec_in_dim = p.hidden_sz
+    p.dec_out_dim = p.hidden_sz
+
+    p.enc_dec_trans_usage = 'consistent'
+    p.enc_dec_trans_act = 'mish'
+    p.enc_dec_trans_forced = True
+
+    p.proj_in_dim = p.emb_sz
+
+    p.enc_dropout = 0
+    p.dec_dropout = 0.5
+    p.enc_attn = "dot_product"
+    p.dec_hist_attn = "none"
+    p.dec_inp_composer = 'cat_mapping'
+    p.dec_inp_comp_activation = 'mish'
+    p.proj_inp_composer = 'cat_mapping'
+    p.proj_inp_comp_activation = 'mish'
+
+    p.decoder_init_strategy = "forward_last_all"
+    p.encoder = 'bilstm'
+    p.use_cell_based_encoder = False
+    # cell-based encoders: typed_rnn, ind_rnn, onlstm, lstm, gru, rnn; see models.base_s2s.base_seq2seq.py file
+    # seq-based encoders: lstm, transformer, bilstm, aug_lstm, aug_bilstm; see models.base_s2s.stacked_encoder.py file
+    p.cell_encoder_is_bidirectional = True     # any cell-based RNN encoder above could be bidirectional
+    p.cell_encoder_uses_packed_sequence = False
+
     return p
 
 
