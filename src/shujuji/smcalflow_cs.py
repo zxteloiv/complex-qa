91c8db3e16b7e8c4e2ec64516519fb98fe5cbf51
zxteloiv <zxteloiv@outlook.com>
2023-06-09 14:35:41 +0800

    sp-struct: add an IID split for the SMCalFlowCS datasets based on the smc128;

--- src/shujuji/smcalflow_cs.py
+++ src/shujuji/smcalflow_cs.py
@@ -16,6 +16,8 @@ def install(reg: dict = None):
     for n in COMP_EX_NUMS:
         reg[f'smc{n}'] = partial(smc_by_num, num=n)
         reg[f'smc{n}_parsed'] = partial(parsed_smc_by_num, num=n)
+    reg[f'smc128iid'] = smc128_iid
+    reg[f'smc128iid_parsed'] = parsed_smc128_iid
 
 
 # the number of compositional examples added into the training set,
@@ -26,8 +28,17 @@ COMP_EX_NUMS = [16, 32, 64, 128]
 
 
 def smc_by_num(num: int):
-    files = ('train.jsonl', 'valid.jsonl', 'test.jsonl')
     path = osp.join(DATA_PATH, f'source_domain_with_target_num{num}')
+    return smc_by_path(path)
+
+
+def smc128_iid():
+    path = osp.join(DATA_PATH, f'source_domain_with_target_num128_iid_shuffled')
+    return smc_by_path(path)
+
+
+def smc_by_path(path: str):
+    files = ('train.jsonl', 'valid.jsonl', 'test.jsonl')
     names = [osp.join(path, f) for f in files]
     return list(map(JsonLDataset, names))
 
@@ -42,6 +53,39 @@ def parsed_smc_by_num(num: int,
                       redis_conn: tuple = REDIS_CONN,
                       redis_expire_sec: int = 0,
                       ):
+    return parsed_smc_by_path(
+        path=osp.join(DATA_PATH, f'source_domain_with_target_num{num}'),
+        redis_pref=f'smcal{num}_',
+        parse_grammar=parse_grammar,
+        start_nonterminal=start_nonterminal,
+        redis_conn=redis_conn,
+        redis_expire_sec=redis_expire_sec,
+    )
+
+
+def parsed_smc128_iid(parse_grammar: str = GRAMMAR_FILE,
+                      start_nonterminal: str = 'start',
+                      redis_conn: tuple = REDIS_CONN,
+                      redis_expire_sec: int = 0,
+                      ):
+    path = osp.join(DATA_PATH, f'source_domain_with_target_num128_iid_shuffled')
+    return parsed_smc_by_path(
+        path=path,
+        redis_pref=f'smcal128iid_',
+        parse_grammar=parse_grammar,
+        start_nonterminal=start_nonterminal,
+        redis_conn=redis_conn,
+        redis_expire_sec=redis_expire_sec,
+    )
+
+
+def parsed_smc_by_path(path: str,
+                       redis_pref: str,
+                       parse_grammar: str = GRAMMAR_FILE,
+                       start_nonterminal: str = 'start',
+                       redis_conn: tuple = REDIS_CONN,
+                       redis_expire_sec: int = 0,
+                       ):
     def _get_ds(file, pref):
         return RedisDataset(
             dataset=LarkParserDatasetWrapper(
@@ -56,10 +100,9 @@ def parsed_smc_by_num(num: int,
         )
 
     files = ('train.jsonl', 'valid.jsonl', 'test.jsonl')
-    train, dev, test = [osp.join(DATA_PATH, f'source_domain_with_target_num{num}', f) for f in files]
+    train, dev, test = [osp.join(path, f) for f in files]
     return (
-        _get_ds(train, f'smcal{num}_train_'),
-        _get_ds(dev, f'smcal{num}_dev_'),
-        _get_ds(test, f'smcal{num}_test_'),
+        _get_ds(train, f'{redis_pref}train_'),
+        _get_ds(dev, f'{redis_pref}dev_'),
+        _get_ds(test, f'{redis_pref}test_'),
     )
-
