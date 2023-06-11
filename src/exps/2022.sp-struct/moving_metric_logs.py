f7e662d5361f945717f7bf2686282cb2ffb5af00
zxteloiv <zxteloiv@outlook.com>
2023-06-11 16:39:28 +0800

    sp-struct: add OT for better llm-metrics;

--- src/exps/2022.sp-struct/moving_metric_logs.py
+++ src/exps/2022.sp-struct/moving_metric_logs.py
@@ -1,11 +1,18 @@
 import math
 import numbers
 import os.path
+import random
+import statistics
 import sys
+from collections import OrderedDict
+from typing import List
+import numpy as np
+
 import pandas as pd
 from scipy.stats import norm
-from collections import OrderedDict
-import json
+
+MAX_ITEMS = 2000
+MAX_TRIALS = 10
 
 
 def main():
@@ -14,7 +21,7 @@ def main():
 
 
 def file_proc(filename, print_header: bool = True):
-    from utils.text_tool import grep, scan, split
+    from utils.text_tool import grep, split
 
     data = grep(open(filename).readlines(), r'Metrics:|:distance:')
     groups = split(data, 'Metrics: ')
@@ -26,7 +33,8 @@ def file_proc(filename, print_header: bool = True):
     param_q = norm.fit(test)
     divergence = kl_for_norms(*param_p, *param_q) + kl_for_norms(*param_q, *param_p)
     _, ds, method, seed = filename.split('.')
-    out = OrderedDict(filename=filename, div=divergence, ds=ds, method=method)
+    method = 'seq' if method == 'moving_metric_flat' else 'prod' if method == 'moving_metric' else method
+    out = OrderedDict(filename=filename, div=divergence, ds=ds, method=method, ot=ot_moving(train, test))
     for k, v in pd.Series(train).describe().items():
         out[f'train_{k}'] = v
     for k, v in pd.Series(test).describe().items():
@@ -45,6 +53,26 @@ def kl_for_norms(m1, s1, m2, s2):
     t1 = ((m2 - m1) / s2) ** 2
     t2 = (s1 / s2) ** 2
     return (t1 + t2 - math.log(t2) - 1) / 2
+
+
+def ot_moving(xs: List[float], xt: List[float]):
+    import ot
+
+    def _moving_dist(small_xs: List[float], small_xt: List[float]):
+        a = np.expand_dims(np.array(small_xs, dtype=np.float64), 1)
+        b = np.expand_dims(np.array(small_xt, dtype=np.float64), 1)
+        return ot.emd2([], [], ot.dist(a, b))
+
+    if len(xs) <= MAX_ITEMS and len(xt) <= MAX_ITEMS:
+        return _moving_dist(xs, xt)
+
+    random.seed(2023)
+    sample = random.sample
+
+    emds = [_moving_dist(sample(xs, MAX_ITEMS) if len(xs) > MAX_ITEMS else xs,
+                         sample(xt, MAX_ITEMS) if len(xt) > MAX_ITEMS else xt)
+            for _ in range(MAX_TRIALS)]
+    return statistics.fmean(emds)
 
 
 if __name__ == '__main__':
