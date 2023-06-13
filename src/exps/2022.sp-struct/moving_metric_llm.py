7b4622668dfb9a4a4c1036ceae81aac3d316c0ae
zxteloiv <zxteloiv@outlook.com>
2023-06-13 21:36:09 +0800

    sp-struct: modified metrics;

--- src/exps/2022.sp-struct/moving_metric_llm.py
+++ src/exps/2022.sp-struct/moving_metric_llm.py
@@ -1,11 +1,18 @@
 import json
+import logging
 import os
+import random
+import statistics
 from functools import partial
 import sys
-import gc
+from typing import List, Tuple, Callable
 
-from trialbot.data import NSVocabulary, RandomIterator
+import lark
+import numpy as np
+from trialbot.data import Dataset
 from trialbot.utils.root_finder import find_root
+from trialbot.training.hparamset import HyperParamSet
+from datetime import datetime as dt
 
 sys.path.insert(0, find_root('.SRC'))
 
@@ -16,187 +23,131 @@ import os.path as osp
 import ot
 
 from shujuji import install_semantic_parsing_datasets, get_field_names_by_prefix
-from utils.trialbot.dummy_translator import install_dummy_translator
 from utils.trialbot.setup_cli import setup as setup_cli
 from utils.s2s_arch.hparam_modifiers import param_overwriting_modifier, install_runtime_modifiers
 from utils.lark.id_tree import build_from_lark_tree
 from utils.tree import Tree, PreOrderTraverse, PostOrderTraverse
 
 
+logger = logging.getLogger()
+
+
 def main():
     install_semantic_parsing_datasets()
 
-    install_dummy_translator()
-
-    args = setup_cli(seed=2021, hparamset='dump')
+    args = setup_cli(seed=2021, hparamset='mm-seq')
 
     install_runtime_modifiers(args.hparamset, partial(param_overwriting_modifier, **dict(
       zip(('src_key', 'tgt_key'), get_field_names_by_prefix(args.dataset))
     )))
-    train, _, _ = Registry.get_dataset(args.dataset)
+    train, dev, test = Registry.get_dataset(args.dataset)
+    p = Registry.get_hparamset(args.hparamset)
+    llm = Embedder(p.chatglm_path, args.device, p.max_tok_num)
+    mm = MovingMetrics(llm, p.num_retries, p.max_dist_size, p.ot_metric_string)
 
-    bot = DumpBot(args, get_model_func=MetricModel.get_model, trial_name='dump-emb', clean_engine=True)
-    bot.run()
+    key_x = p.src_key
+    key_y = p.tgt_key + '_tree' if p.use_tgt_trees else p.tgt_key
 
+    # potentially-large lists of either strings or trees
+    train_x, train_y = get_distributions(train, key_x, key_y)
+    # dev_x, dev_y = get_distributions(dev, key_x, key_y)
+    test_x, test_y = get_distributions(test, key_x, key_y)
+    logger.info(f'data size: x/x_: {len(train_x)}/{len(test_x)}')
+    logger.info(f'data size: y/y_: {len(train_y)}/{len(test_y)}')
 
-class DumpBot(TrialBot):
-    def _init_vocab(self, dataset, translator):
-        return NSVocabulary()
-
-    def run(self, training_epoch: int = 0):
-        self.model.eval()
-        self.logger.info(f'writing dump file train-dump.jsonl at {self.savepath}')
-        self.dump_ds(self.train_set, 'train-dump.jsonl')
-        self.logger.info(f'writing dump file dev-dump.jsonl at {self.savepath}')
-        self.dump_ds(self.dev_set, 'dev-dump.jsonl')
-        self.logger.info(f'writing dump file test-dump.jsonl at {self.savepath}')
-        self.dump_ds(self.test_set, 'test-dump.jsonl')
-
-    def dump_ds(self, ds, dump_filename: str):
-        iterator = RandomIterator(len(ds), self.hparams.batch_sz, False, False)
-        if not osp.exists(self.savepath):
-            os.makedirs(self.savepath, mode=0o755)
-
-        fout = open(osp.join(self.savepath, dump_filename), 'wt')
-        total_iter = total_xs = 0
-        for bi, indices in enumerate(iterator):
-            xs = self.translator.batch_tensor([
-                self.translator.to_tensor(ds[i])
-                for i in indices
-            ])
-            if len(xs) > 0:
-                model: MetricModel = self.model
-                output: dict = model.dump(**xs)
-                keys, vals = output.keys(), output.values()
-
-                for tp in zip(*vals):
-                    line_obj = dict(zip(keys, tp))
-                    fout.write(json.dumps(line_obj))
-                    fout.write('\n')
-
-                item_len = len(next(iter(xs.values())))
-            else:
-                item_len = 0
-
-            total_xs += item_len
-            total_iter += len(indices)
-            self.logger.info(f'Dumped the batch {bi} len={item_len}/{len(indices)}')
-            if bi % 10 == 0:
-                fout.flush()
-
-            gc.collect()
-            if self.args.device >= 0:
-                import torch.cuda
-                torch.cuda.empty_cache()
-
-        fout.close()
-        self.logger.info(f'total-dumped: {total_xs} / {total_iter}')
+    with torch.inference_mode():
+        logger.info(f'compute xy {timestr("%H:%M:%S")}')
+        xy = mm.compute(train_x, train_y, parallel=True)
+        logger.info(f'compute xx_ {timestr("%H:%M:%S")}')
+        xx_ = mm.compute(train_x, test_x)
+        logger.info(f'compute yy_ {timestr("%H:%M:%S")}')
+        yy_ = mm.compute(train_y, test_y)
+        logger.info(f'compute x_y_ {timestr("%H:%M:%S")}')
+        x_y_ = mm.compute(test_x, test_y, parallel=True)
+        logger.info(f'completed {timestr("%H:%M:%S")}')
+        print(xy, xx_, yy_, x_y_)
 
 
-class MetricModel(torch.nn.Module):
-    @classmethod
-    def get_model(cls, p, vocab):
-        return cls(p.chatglm_path, p.src_key, p.tgt_key, p.use_tgt_trees, p.max_tok_num, p.only_compare_nt)
+def get_distributions(ds: Dataset, key_x: str, key_y: str):
+    xs = []
+    ys = []
+    for example in ds:
+        x = example.get(key_x)
+        y = example.get(key_y)
+        if x is not None and y is not None:
+            xs.append(x)
+            ys.append(y)
+    return xs, ys
 
-    def __init__(self, llm_path: str, src_key: str, tgt_key: str,
-                 use_tgt_trees: bool = True,
-                 max_toks_num: int = 10000,
-                 only_compare_nt_nodes: bool = True,
+
+class Embedder:
+    def __init__(self,
+                 llm_path: str = '~/.glm/chatglm-6b',
+                 device: int = -1,
+                 max_tok_num: int = 1100,
                  ):
-        super().__init__()
+        llm_path = osp.expanduser(llm_path)
         self.tok = AutoTokenizer.from_pretrained(llm_path, trust_remote_code=True, revision='v0.1.0')
-        self.model = AutoModel.from_pretrained(llm_path, trust_remote_code=True, revision='v0.1.0').half().eval()
-        self.src_key = src_key
-        self.tgt_key = tgt_key
-        self.use_tgt_trees = use_tgt_trees
-        self.max_toks_num = max_toks_num
-        self.only_compare_nt_nodes = only_compare_nt_nodes
+        self.model = AutoModel.from_pretrained(llm_path, trust_remote_code=True, revision='v0.1.0').half()
+        self.model.eval()
+        self.device = self.int_to_device(device)
+        if device >= 0:
+            self.model.cuda(device)
+        self.max_tok_num = max_tok_num
+
+    @staticmethod
+    def int_to_device(device):
+        if isinstance(device, torch.device):
+            return device
+        if device < 0:
+            return torch.device("cpu")
+        return torch.device(device)
 
     @torch.inference_mode()
-    def dump(self, **kwargs):
-        src = kwargs[self.src_key]
-        tgt_trees = [self.compute_offsets(build_from_lark_tree(t))
-                     for t in kwargs[self.tgt_key + '_tree'] if t is not None]
-        if len(tgt_trees) == 0:
-            return
-        tgt = self.restore_text_from_trees(tgt_trees)
-        src_emb, src_ids = self.embed(src)
-        tgt_emb, tgt_ids = self.embed(tgt)
-        output = dict(src_emb=src_emb.tolist(), src_ids=src_ids.tolist(),
-                      tgt_emb=tgt_emb.tolist(), tgt_ids=tgt_ids.tolist(),
-                      src=src, tgt=tgt,)
-        return output
-
-    @torch.inference_mode()
-    def forward(self, **kwargs):
-        # list of str, or trees
-        torch.no_grad()
-        src = kwargs[self.src_key]
-
-        if self.use_tgt_trees:
-            tgt_trees = [self.compute_offsets(build_from_lark_tree(t))
-                         for t in kwargs[self.tgt_key + '_tree'] if t is not None]
-            if len(tgt_trees) == 0:
-                return
-
-            tgt = self.restore_text_from_trees(tgt_trees)
-            child_fn = Tree.get_nt_children_fn() if self.only_compare_nt_nodes else None
-            offset_of_trees: list = [[node.payload for node in PreOrderTraverse(child_fn)(t)]
-                                     for t in tgt_trees]
-        else:
-            tgt = kwargs[self.tgt_key]
-            offset_of_trees = []
-
-        # (batch, len(s or t), dim), (batch, len(s or t))
-        src_emb, src_ids = self.embed(src)
-        tgt_emb, tgt_ids = self.embed(tgt)
-
-        if self.use_tgt_trees:
-            it = zip(src_emb, src_ids, tgt_emb, tgt_ids, offset_of_trees)
-            for x_emb, x_ids, y_emb, y_ids, y_offsets in it:
-                x_node_emb = self.compute_flat_seq_emb(x_emb, x_ids).double()
-                y_node_emb = self.compute_tree_style_emb(y_ids, y_emb, y_offsets).double()
-                self.compute_metric(x_node_emb, y_node_emb)
-
-        else:
-            it = zip(src_emb, src_ids, tgt_emb, tgt_ids)
-            for x_emb, x_ids, y_emb, y_ids in it:
-                x_node_emb = self.compute_flat_seq_emb(x_emb, x_ids).double()
-                y_node_emb = self.compute_flat_seq_emb(y_emb, y_ids).double()
-                self.compute_metric(x_node_emb, y_node_emb)
-
-    def infer_device(self) -> torch.device:
-        return next(self.model.parameters()).device
-
-    def embed(self, xs: list):
+    def embed(self, xs: List[str]) -> Tuple[torch.DoubleTensor, torch.IntTensor]:
         xs_inputs = self.tok(xs,
                              padding=True,
-                             return_tensors='pt').to(self.infer_device())
+                             return_tensors='pt').to(self.device)
         xs_out = self.model(**xs_inputs, output_hidden_states=True)
-        xs_emb = xs_out.hidden_states[-1].transpose(0, 1)
+        xs_emb = xs_out.hidden_states[-1].transpose(0, 1).double()
         xs_ids = xs_inputs['input_ids']
-        return xs_emb, xs_ids   # (batch, len, dim)
+        return xs_emb, xs_ids   # (batch, len, dim), (batch, len)
 
-    def compute_metric(self, x_node_emb, y_node_emb):
-        """
-        :param x_node_emb: (num1, dim)
-        :param y_node_emb: (num2, dim)
-        :return:
-        """
-        num_x_node, num_y_node = x_node_emb.size(0), y_node_emb.size(0)
-        dev = x_node_emb.device
+    @torch.inference_mode()
+    def __call__(self, strings_or_trees: list) -> torch.Tensor:
+        if len(strings_or_trees) == 0:
+            raise ValueError('Empty list can not be embedded.')
 
-        Mf = ot.dist(x_node_emb, y_node_emb, metric='euclidean')
+        elem = strings_or_trees[0]
+        if isinstance(elem, str):
+            strings = list(filter(None, strings_or_trees))
+            xs_emb, xs_id = self.embed(strings)
+            return self.compute_flat_seq_emb(xs_emb, xs_id)
 
-        x_uni = torch.ones((num_x_node,), device=dev, dtype=torch.float64) / num_x_node
-        y_uni = torch.ones((num_y_node,), device=dev, dtype=torch.float64) / num_y_node
+        elif isinstance(elem, lark.Tree):
+            trees = filter(None, strings_or_trees)
+            trees = [self.compute_offsets(build_from_lark_tree(t)) for t in trees]
+            trees_offset = [[node.payload for node in PreOrderTraverse()(t)] for t in trees]
+            terms: List[str] = self.restore_text_from_trees(trees)
+            terms_emb, terms_id = self.embed(terms)
+            return self.compute_tree_style_emb(terms_id, terms_emb, trees_offset)
 
-        dist = ot.emd2(x_uni, y_uni, Mf)
+        else:
+            raise TypeError('Invalid list. Only lists of strings or trees in Lark are supported.')
 
-        del Mf, x_uni, y_uni, x_node_emb, y_node_emb
-        self.logger.info(f'distance: {dist.item()}')
-        self.avg_metric(dist)
+    @staticmethod
+    def compute_offsets(tree):
+        running_prefix = 0
+        for node in PostOrderTraverse()(tree):
+            if node.is_terminal:
+                node.payload = (running_prefix, running_prefix + len(node.label))
+                running_prefix += 1 + len(node.label)  # the space sep
+            else:
+                # use the left-most to right-most
+                node.payload = (node.children[0].payload[0], node.children[-1].payload[-1])
+        return tree
 
+    @torch.inference_mode()
     def compute_flat_seq_emb(self, emb, x_ids):
         """
         :param emb: (padded_tok_num, dim)
@@ -205,15 +156,12 @@ class MetricModel(torch.nn.Module):
         """
         mask = (x_ids > 20004).unsqueeze(-1)   # printable tokens starts from 20005
         hid_sz = emb.size(-1)
-        selected_embs = torch.masked_select(emb, mask).reshape(-1, hid_sz)[:self.max_toks_num]
+        selected_embs = torch.masked_select(emb, mask).reshape(-1, hid_sz)[:self.max_tok_num]
         sent_emb = selected_embs.mean(0).unsqueeze(0)   # (1, dim)
-        if self.only_compare_nt_nodes:
-            src_emb = sent_emb
-        else:
-            src_emb = torch.cat([selected_embs, sent_emb], dim=0)   # (sel_toks + 1, dim)
-        del selected_embs, sent_emb, mask
+        src_emb = torch.cat([selected_embs, sent_emb], dim=0)   # (sel_toks + 1, dim)
         return src_emb
 
+    @torch.inference_mode()
     def compute_tree_style_emb(self, term_ids, term_embs, tree_offsets):
         """
         Assuming a tree NT node is represented as the mean of the span.
@@ -224,7 +172,7 @@ class MetricModel(torch.nn.Module):
         """
         len_contrib: list = [len(self.tok.decode(term_ids[:i+1]))
                              for i in range(len(term_ids))
-                             if i < self.max_toks_num]
+                             if i < self.max_tok_num]
 
         def _find_tok_id(n, start=0) -> int:
             if n >= len_contrib[-1]:
@@ -250,18 +198,6 @@ class MetricModel(torch.nn.Module):
         return tgt_emb  # (num_nodes, dim)
 
     @staticmethod
-    def compute_offsets(tree):
-        running_prefix = 0
-        for node in PostOrderTraverse()(tree):
-            if node.is_terminal:
-                node.payload = (running_prefix, running_prefix + len(node.label))
-                running_prefix += 1 + len(node.label)  # the space sep
-            else:
-                # use the left-most to right-most
-                node.payload = (node.children[0].payload[0], node.children[-1].payload[-1])
-        return tree
-
-    @staticmethod
     def restore_text_from_trees(t_or_ts):
         def _restore(t):
             return ' '.join(node.label for node in PreOrderTraverse()(t) if node.is_terminal)
@@ -271,29 +207,124 @@ class MetricModel(torch.nn.Module):
         else:
             return [_restore(t) for t in t_or_ts]
 
-    def get_metrics(self, reset: bool = False):
-        return {"DIST": self.avg_metric.get_metric(reset=reset)}
+
+class MovingMetrics:
+    def __init__(self,
+                 embed: Embedder,
+                 num_retries: int = 0,
+                 max_elem_num: int = 0,
+                 ot_metric_string: str = 'euclidean',
+                 ):
+        self.num_retries: int = num_retries
+        self.max_elem_num: int = max_elem_num
+        self.metric = ot_metric_string
+        self.embed = embed
+
+    def clear_cache(self):
+        if self.embed.device.type != 'cpu':
+            torch.cuda.empty_cache()
+
+    def cost_on_distributions(self, xs: list, xt: list):
+        n, m = len(xs), len(xt)
+        logger.info(f'distribution costs estimating: {n}/{m}. {timestr()}')
+        costs = torch.zeros(n, m, device=self.embed.device, dtype=torch.double)
+        for i in range(n):
+            for j in range(m):
+                cost_ij = self.direct_metric(xs[i], xt[j])
+                costs[i, j] = cost_ij
+                del cost_ij
+        return costs
+
+    def direct_metric(self, xs, xt, parallel: bool = False) -> torch.Tensor:
+        """
+        Compute the metric. Exceptions like scales and lengths are assured absent.
+        Both distribution must be in the same metric space.
+        :param xs: list of (str or lark.Tree)
+        :param xt: list of (str or lark.Tree)
+        :param parallel: bool, if True, the xs and xt are parallel samples,
+                        computing the average metric for each pair of them is just fine.
+                        This will save a lot computations.
+        :return:
+        """
+        logger.info(f'direct metric: sizes={len(xs)}/{len(xt)}, parallel={parallel}: {timestr()}')
+        if isinstance(xs, list) and parallel:
+            pairwise_metrics = []
+            for a, b in zip(xs, xt):
+                pairwise_metrics.append(self.direct_metric(a, b, parallel=False))
+            return sum(pairwise_metrics) / len(pairwise_metrics)
+
+        if isinstance(xs, list):
+            costs = self.cost_on_distributions(xs, xt)
+        else:
+            xs_emb: torch.Tensor = self.embed(xs)
+            xt_emb: torch.Tensor = self.embed(xt)
+            costs = ot.dist(xs_emb, xt_emb, metric=self.metric)    # perhaps a tensor on cuda
+
+        self.clear_cache()
+        return ot.emd2(torch.tensor([]), torch.tensor([]), costs)   # a torch scalar
+
+    def sample(self, *args):   # sample from all sources based on the first
+        if len(args[0]) <= self.max_elem_num:
+            if len(args) == 1: return args[0]
+            else: return args
+        else:
+            indices = random.sample(range(len(args[0])), k=self.max_elem_num)
+            if len(args) == 1: return [args[0][i] for i in indices]
+            else: return tuple([a[i] for i in indices] for a in args)
+
+    def compute(self, xs: list, xt: list, parallel: bool = False) -> float:
+        if len(xs) <= 0 or len(xt) <= 0:
+            raise ValueError('Input samples must not be empty.')
+
+        if parallel and len(xs) != len(xt):
+            raise ValueError('Input samples are of different size and thus not parallel.')
+
+        if self.max_elem_num <= 0:  # the option is valid only when it's positive
+            return self.direct_metric(xs, xt, parallel=parallel).item()
+
+        # when both distributions are small, return the exact direct metric.
+        if len(xs) <= self.max_elem_num and len(xt) <= self.max_elem_num:
+            return self.direct_metric(xs, xt, parallel=parallel).item()
+
+        metrics = []
+        for turn in range(self.num_retries):
+            if parallel:  # in parallel both xs and xt are bounded in size.
+                xss, xtt = self.sample(xs, xt)
+
+            else:   # decoupled sampling
+                xss = self.sample(xs)
+                xtt = self.sample(xt)
+
+            metrics.append(self.direct_metric(xss, xtt, parallel=parallel).item())
+
+        return statistics.fmean(metrics)
 
 
-@Registry.hparamset('tree-nt-moving')
+def timestr(fmt='%H:%M:%S'):
+    return dt.now().strftime(fmt)
+
+
+@Registry.hparamset('mm-seq')
 def base():
-    from trialbot.training.hparamset import HyperParamSet
-    from trialbot.utils.root_finder import find_root
-    p = HyperParamSet.common_settings(find_root())
+    p = HyperParamSet()
+
+    # embedder params
     p.chatglm_path = osp.expanduser('~/.glm/chatglm-6b')
-    p.TRANSLATOR = 'dummy'
-    p.TRANSLATOR_KWARGS = dict(filter_none=True)
-    p.TRAINING_LIMIT = 1
-    p.batch_sz = 4  # must be small, otherwise the GPU memory will goes run out.
-    p.use_tgt_trees = True
     p.max_tok_num = 1100    # >num of 99% examples on ATIS
-    p.only_compare_nt = True
+
+    # moving-metric params
+    p.num_retries = 5
+    p.max_dist_size = 200
+    p.ot_metric_string = 'euclidean'
+
+    p.use_tgt_trees = False
     return p
 
 
-@Registry.hparamset('dump')
+@Registry.hparamset('mm-prod')
 def dump():
     p = base()
+    p.use_tgt_trees = True
     return p
 
 
