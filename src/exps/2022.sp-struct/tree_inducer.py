ef9331611baa2cf7c40f2b0536542572ba2b7fba
zxteloiv <zxteloiv@outlook.com>
2023-06-24 16:09:58 +0800

    sp-struct: tree-inducer uses smaller sampling size, and changed to logpp1-based optimization;

--- src/exps/2022.sp-struct/tree_inducer.py
+++ src/exps/2022.sp-struct/tree_inducer.py
@@ -1,8 +1,10 @@
+import logging
+import math
 import os
 from functools import partial
 from operator import itemgetter
 from statistics import fmean, stdev
-from typing import Tuple, Union, List
+from typing import Tuple, Union, List, Dict, Any
 
 import ot
 import torch.nn
@@ -21,8 +23,8 @@ if SRC_PATH not in sys.path:
 from shujuji import install_semantic_parsing_datasets, get_field_names_by_prefix
 from utils.trialbot.dummy_translator import install_dummy_translator
 from utils.trialbot.setup_cli import setup as setup_cli
+from utils.trialbot.setup_bot import setup_bot
 from utils.select_optim import select_optim
-from utils.s2s_arch.hparam_modifiers import install_runtime_modifiers, param_overwriting_modifier
 from utils.tree import Tree, PreOrderTraverse, PostOrderTraverse
 
 from moving_metric_llm import MovingMetrics, Embedder
@@ -38,16 +40,17 @@ def main():
 
     bot = TrialBot(args, 'tree_inducer', get_model)
 
-    @bot.attach_extension(Events.STARTED)
-    def print_llm(bot):
-        bot.logger.info("LLM Spec:\n" + str(bot.embedder.model))
-        bot.logger.info("Tokenizer Spec:\n" + str(bot.embedder.tok))
-
     # init additional components
     ds_field_names = get_field_names_by_prefix(args.dataset)
     bot.translator.gather_keys = ds_field_names
     bot.embedder = get_moving_metric(bot)
     bot.updater = MyUpdater.from_bot(bot)
+    bot = setup_bot(bot, True, False, False, False, False, True)
+
+    @bot.attach_extension(Events.STARTED)
+    def print_llm(bot):
+        bot.logger.info("LLM Spec:\n" + str(bot.embedder.model))
+        bot.logger.info("Tokenizer Spec:\n" + str(bot.embedder.tok))
 
     bot.run()
 
@@ -58,7 +61,7 @@ def get_model(p, vocab):
         torch.nn.Linear(4096, p.hid_sz),
         torch.nn.Mish(),
         torch.nn.Linear(p.hid_sz, 1),
-        torch.nn.LogSigmoid()
+        torch.nn.Sigmoid()
     )
 
 
@@ -79,26 +82,37 @@ class MyUpdater(Updater):
         updater.bot = bot   # save the backward reference for convenience
         updater.num_turns = p.num_turns
         updater.num_k = p.num_tree_sampling
+        updater.grad_clip = p.GRAD_CLIPPING
+        updater.logger = logging.getLogger(cls.__name__)
         return updater
 
     def update_epoch(self):
         datasets = self.bot.datasets    # noqa
 
-        # sample data, fixed during each tree sampling
-        train_samples, test_samples = [], []
+        # sample and embed data, which is fixed during each time of tree sampling
+        train_turns, test_turns = [], []
         for turn in range(self.num_turns):
-            train_samples.append(self.get_samples(datasets[0], self._iterators[0]))
-            test_samples.append(self.get_samples(datasets[-1], self._iterators[-1]))
+            train_samples = self.get_samples(datasets[0], self._iterators[0])
+            train_turns.append(self.embed_data(*train_samples))
 
-        if self._iterators[-1].is_end_of_epoch:
+            test_samples = self.get_samples(datasets[-1], self._iterators[-1])
+            test_turns.append(self.embed_data(*test_samples))
+
+        self.logger.info('finished embedding the samples of every turn.')
+
+        if self._iterators[0].is_end_of_epoch:
             self.stop_epoch()
 
         optim = self._optims[0]
         optim.zero_grad()
         losses = []
         for k in range(self.num_k):
-            loss_k = self.once_tree_sampling(train_samples, test_samples)
+            loss_k = self.once_tree_sampling(train_turns, test_turns)
+            self.logger.info(f'conducted the {k}th tree sampling with loss={loss_k}')
             losses.append(loss_k)
+
+        if self.grad_clip > 0:
+            torch.nn.utils.clip_grad_value_(self.bot.model.parameters(), self.grad_clip)
         optim.step()
         return {'loss': sum(losses)}
 
@@ -109,42 +123,63 @@ class MyUpdater(Updater):
         key_pairs = get_field_names_by_prefix(self.bot.args.dataset)
         return itemgetter(*key_pairs)(samples)
 
-    def once_tree_sampling(self, train_samples, test_samples) -> float:
-        logp_list, reward = self.single_tree(train_samples, test_samples)
-        loss = - sum(logp_list) * reward
+    def embed_data(self, x_data, y_data):
+        list_of_dicts = []
+
+        for x, y in zip(x_data, y_data):
+            embedder: Embedder = self.bot.embedder    # noqa
+
+            x_emb, x_id = embedder.embed(x)  # (num_toks, emb)
+            x_emb = x_emb.float().clone().detach_()
+            sx_emb = embedder.compute_flat_seq_emb(x_emb, x_id)
+
+            y_emb, y_id = embedder.embed(y)  # (num_toks, emb)
+            y_emb = y_emb.float().clone().detach_()
+
+            list_of_dicts.append(dict(x=x, x_id=x_id.tolist(), x_emb=x_emb, sx_emb=sx_emb,
+                                      y=y, y_id=y_id.tolist(), y_emb=y_emb))
+        return list_of_dicts
+
+    def once_tree_sampling(self,
+                           train_turns: List[List[Dict[str, Any]]],
+                           test_turns: List[List[Dict[str, Any]]],
+                           ) -> float:
+        train_mms, test_mms = [], []
+        total_logp_list: List[torch.Tensor] = []
+
+        for turn in range(self.num_turns):
+            train_samples = train_turns[turn]
+            test_samples = test_turns[turn]
+
+            logp_list, mm = self.xy_rv_mm(train_samples)
+            total_logp_list += logp_list
+            train_mms.append(mm)
+
+            logp_list, mm = self.xy_rv_mm(test_samples)
+            total_logp_list += logp_list
+            test_mms.append(mm)
+
+            self.logger.debug(f'turn {turn + 1} received the train/test mm:'
+                              f'{train_mms[-1]}/{test_mms[-1]}')
+
+        if self.num_turns < 2:
+            reward = abs(fmean(train_mms) - fmean(test_mms))
+        else:
+            mean_train = fmean(train_mms)
+            mean_test = fmean(test_mms)
+            std_train = stdev(train_mms, mean_train)
+            std_test = stdev(test_mms, mean_test)
+            reward = abs(mean_train - mean_test) / std_train / std_test
+
+        # the lower reward the better.
+        loss = sum(total_logp_list) * reward
         loss.backward()
         return loss.item()
 
-    def single_tree(self, train_data, test_data) -> Tuple[List[torch.Tensor], float]:
-        if self.num_turns == 1:
-            train_logp_list, train_mm = self.single_turn(*train_data[0])
-            test_logp_list, test_mm = self.single_turn(*test_data[0])
-            reward = abs(train_mm - test_mm)
-            return train_logp_list + test_logp_list, reward
-
-        logp_list = []
-        train_mm_list, test_mm_list = [], []
-        for turn in range(self.num_turns):
-            turn_logp_list, train_mm = self.single_turn(*train_data[turn])
-            logp_list.extend(turn_logp_list)
-            train_mm_list.append(train_mm)
-
-            turn_logp_list, test_mm = self.single_turn(*test_data[turn])
-            logp_list.extend(turn_logp_list)
-            test_mm_list.append(test_mm)
-
-        mean_train = fmean(train_mm_list)
-        mean_test = fmean(test_mm_list)
-        std_train = stdev(train_mm_list)
-        std_test = stdev(test_mm_list)
-
-        reward = abs(mean_train - mean_test) / std_train / std_test
-        return logp_list, reward
-
-    def single_turn(self, xs, ys) -> Tuple[list, float]:
+    def xy_rv_mm(self, samples: List[Dict[str, Any]]) -> Tuple[list, float]:
         logp_list, distances = [], []
-        for x, y in zip(xs, ys):
-            tree_logp, dist = self.single_example(x, y)
+        for sample in samples:
+            tree_logp, dist = self.single_example(sample)
             logp_list.append(tree_logp)
             distances.append(dist)
 
@@ -152,21 +187,17 @@ class MyUpdater(Updater):
         xy_mm = sum(distances) / len(distances)
         return logp_list, xy_mm
 
-    def single_example(self, x, y) -> Tuple[torch.Tensor, float]:
-        embedder: Embedder = self.bot.embedder    # noqa
+    def single_example(self, sample: Dict[str, Any]) -> Tuple[torch.Tensor, float]:
+        y_emb = sample['y_emb']
+        prob_net = self._models[0]
+        y_term_prob = prob_net(y_emb)  # (num_toks, 1)
+        y_id_str = [str(x) for x in sample['y_id']]
 
-        x_emb, x_id = embedder.embed(x)  # (num_toks, emb)
-        x_emb = x_emb.float().clone()
-        sx_emb = embedder.compute_flat_seq_emb(x_emb, x_id)
+        tree_logp, tree = sample_a_tree(y_id_str, y_term_prob.squeeze(-1))
+        ty_emb = get_tree_emb(y_emb, tree).double()
 
-        y_emb, y_id = embedder.embed(y)  # (num_toks, emb)
-        y_emb = y_emb.float().clone()
-        logit_net = self._models[0]
-        y_contrib_logp = logit_net(y_emb)    # (num_toks, 1)
-        tree_logp, tree = sample_a_tree([str(x) for x in y_id.tolist()], y_contrib_logp.squeeze(-1))
-        ty_emb = get_tree_emb(y_emb, tree)
-
-        dist = ot.dist(sx_emb.double(), ty_emb.double(), metric='euclidean')
+        sx_emb = sample['sx_emb'].double()
+        dist = ot.dist(sx_emb, ty_emb, metric='euclidean')
         null_t = torch.tensor([])
         return tree_logp, ot.emd2(null_t, null_t, dist).item()
 
@@ -195,11 +226,11 @@ def get_tree_emb(term_embs: torch.Tensor, tree: Tree):
     return tgt_emb  # (num_nodes, dim)
 
 
-def sample_a_tree(terms, logp,
+def sample_a_tree(terms, probs,
                   span_begin: int = None,
                   span_end: int = None,) -> Tuple[torch.Tensor, Tree]:
     """
-    :param logp:  (num_toks,)
+    :param probs:  (num_toks,)
     :param terms:   [str(id) of terms] len=num_toks
     :param span_begin: start of span for a tree
     :param span_end: end of span for a tree, not included
@@ -211,19 +242,21 @@ def sample_a_tree(terms, logp,
 
     length = span_end - span_begin
     if length <= 3:
-        return 0, Tree(nt_label, children=[ # noqa
+        return math.log(2), Tree(nt_label, children=[ # noqa
             Tree(terms[span_begin + i], is_terminal=True) for i in range(length)
         ])
 
-    density = logp[span_begin:span_end].exp()
-    be_terms = density.bernoulli()   # (span_length,)
+    span_p = probs[span_begin:span_end]
+    be_terms = span_p.bernoulli()   # (span_length,)
     i = 0
     while be_terms.sum() < 1:
         del be_terms
         i += .1
-        be_terms = (density + i).clamp(max=.99).bernoulli()  # (span_length,)
+        be_terms = (span_p + i).clamp(max=.99).bernoulli()  # (span_length,)
 
-    level_logp = (be_terms * logp[span_begin:span_end]).sum()
+    level_logp = (be_terms * (span_p + 1).log()
+                  + (1 - be_terms) * (2 - span_p).log()).sum()
+
     terminal_locs = torch.argwhere(be_terms).squeeze(-1) + span_begin  # (num_immediate_children,)
     del be_terms
 
@@ -233,13 +266,13 @@ def sample_a_tree(terms, logp,
         # consider possible subtrees / nonterminals before the current terminal
         if i == 0 and loc > span_begin:
             # there's a subtree before the first terminal
-            subtree_logp, subtree = sample_a_tree(terms, logp, span_begin, loc)
+            subtree_logp, subtree = sample_a_tree(terms, probs, span_begin, loc)
             sub_logp.append(subtree_logp)
             children.append(subtree)
 
         elif loc > terminal_locs[i - 1] + 1:
             # a subtree between last and this terminals
-            subtree_logp, subtree = sample_a_tree(terms, logp, terminal_locs[i - 1] + 1, loc)
+            subtree_logp, subtree = sample_a_tree(terms, probs, terminal_locs[i - 1] + 1, loc)
             sub_logp.append(subtree_logp)
             children.append(subtree)
 
@@ -248,7 +281,7 @@ def sample_a_tree(terms, logp,
 
         # a subtree after the last terminal, add it before ending the loop
         if i == len(terminal_locs) - 1 and loc < span_end - 1:
-            subtree_logp, subtree = sample_a_tree(terms, logp, loc + 1, span_end)
+            subtree_logp, subtree = sample_a_tree(terms, probs, loc + 1, span_end)
             sub_logp.append(subtree_logp)
             children.append(subtree)
 
@@ -261,14 +294,15 @@ def sample_a_tree(terms, logp,
 def base():
     p = HyperParamSet.common_settings(find_root('.ROOT'))
     p.OPTIM = 'adabelief'
+    p.TRAINING_LIMIT = 100
     p.llm_path = osp.expanduser('~/.glm/chatglm-6b')
     p.max_tok_num = 1100
     p.hid_sz = 1024
     p.use_causal_lm_cls = False # ChatGLM uses AutoModel instead of AutoModelForCausalLM
 
-    p.batch_sz = 50
-    p.num_turns = 10
-    p.num_tree_sampling = 5
+    p.batch_sz = 10
+    p.num_turns = 5
+    p.num_tree_sampling = 3
     p.ADAM_LR = 1e-3
     return p
 
