30bab7466abb39fb5038e5fce7d6f05a77bc4c2d
zxteloiv <zxteloiv@outlook.com>
2023-06-25 14:09:10 +0800

    sp-struct: add an inducer evaluator and dump the trees;

--- src/exps/2022.sp-struct/tree_inducer.py
+++ src/exps/2022.sp-struct/tree_inducer.py
@@ -1,10 +1,12 @@
 import logging
 import math
 import os
+import pickle
+from collections import defaultdict
 from functools import partial
 from operator import itemgetter
 from statistics import fmean, stdev
-from typing import Tuple, Union, List, Dict, Any
+from typing import Tuple, Union, List, Dict, Any, Callable, Literal
 
 import ot
 import torch.nn
@@ -26,6 +28,7 @@ from utils.trialbot.setup_cli import setup as setup_cli
 from utils.trialbot.setup_bot import setup_bot
 from utils.select_optim import select_optim
 from utils.tree import Tree, PreOrderTraverse, PostOrderTraverse
+from utils.seq_collector import SeqCollector
 
 from moving_metric_llm import MovingMetrics, Embedder
 
@@ -39,20 +42,48 @@ def main():
     args = setup_cli(device=0, seed=2021, translator='dummy', hparamset='base')
 
     bot = TrialBot(args, 'tree_inducer', get_model)
-
-    # init additional components
     ds_field_names = get_field_names_by_prefix(args.dataset)
     bot.translator.gather_keys = ds_field_names
+    # init additional components
     bot.embedder = get_moving_metric(bot)
-    bot.updater = InducerTrainingUpdater.from_bot(bot)
-    bot = setup_bot(bot, True, False, False, False, False, True)
 
     @bot.attach_extension(Events.STARTED)
     def print_llm(bot):
         bot.logger.info("LLM Spec:\n" + str(bot.embedder.model))
         bot.logger.info("Tokenizer Spec:\n" + str(bot.embedder.tok))
 
+    if args.test:
+        eval_inducer(args, bot)
+    else:
+        train_inducer(args, bot)
+
+
+def train_inducer(args, bot: TrialBot):
+    bot.updater = InducerTrainingUpdater.from_bot(bot)
+    bot = setup_bot(bot, True, False, False, False, False, True)
     bot.run()
+
+
+def eval_inducer(args, bot):
+    bot.updater = InducerEvalUpdater.from_bot(bot)
+    bot = setup_bot(bot, False, False, False, False, False, True)
+    trees = defaultdict(list)
+    ys = defaultdict(list)
+
+    @bot.attach_extension(Events.ITERATION_COMPLETED)
+    def collect_trees(bot: TrialBot):
+        output = bot.state.output
+        ds_id = bot.updater.ds_id
+        trees[ds_id] += output['y_tree']
+        ys[ds_id] += output['y']
+
+    bot.updater.set_inducing_ds('train')
+    bot.run()
+
+    bot.updater.set_inducing_ds('test')
+    bot.run()
+
+    pickle.dump({"ys": ys, "trees": trees}, open('output.pkl', 'wb'))
 
 
 def get_model(p, vocab):
@@ -123,7 +154,7 @@ class InducerTrainingUpdater(Updater):
         key_pairs = get_field_names_by_prefix(self.bot.args.dataset)
         return itemgetter(*key_pairs)(samples)
 
-    def embed_data(self, x_data, y_data):
+    def embed_data(self, x_data: List[str], y_data: List[str]) -> List[Dict[str, Any]]:
         list_of_dicts = []
 
         for x, y in zip(x_data, y_data):
@@ -193,13 +224,60 @@ class InducerTrainingUpdater(Updater):
         y_term_prob = prob_net(y_emb)  # (num_toks, 1)
         y_id_str = [str(x) for x in sample['y_id']]
 
-        tree_logp, tree = sample_a_tree(y_id_str, y_term_prob.squeeze(-1))
+        tree_logp, tree = TreeSampler().sample(y_id_str, y_term_prob.squeeze(-1))
         ty_emb = get_tree_emb(y_emb, tree).double()
 
         sx_emb = sample['sx_emb'].double()
         dist = ot.dist(sx_emb, ty_emb, metric='euclidean')
         null_t = torch.tensor([])
         return tree_logp, ot.emd2(null_t, null_t, dist).item()
+
+
+class InducerEvalUpdater(InducerTrainingUpdater):
+    @classmethod
+    def from_bot(cls, bot: TrialBot) -> 'InducerEvalUpdater':
+        args, p = bot.args, bot.hparams
+        updater = super().from_bot(bot)
+        iterators = [RandomIterator(len(d), p.batch_sz, False, False) for d in bot.datasets]
+        updater._iterators = iterators
+        updater._optims = []
+        updater.ds_id = 0   # train by default
+        return updater
+
+    def set_inducing_ds(self, split: Literal['train', 'dev', 'test']):
+        splits = ('train', 'dev', 'test')
+        if split not in splits:
+            raise ValueError(f'unsupported target dataset {split}, only {splits} available')
+        self.ds_id = splits.index(split)
+
+    def update_epoch(self):
+        bot: TrialBot = self.bot # noqa
+        bot.model.eval()
+        ds = bot.datasets[self.ds_id]
+        iter = self._iterators[self.ds_id]
+        if iter.is_end_of_epoch:
+            self.stop_epoch()
+
+        samples: Tuple[List[str], List[str]] = self.get_samples(ds, iter)
+        output: Dict[str, List[Any]] = self.induce_from_samples(samples)
+        return output
+
+    def induce_from_samples(self, samples: Tuple[List[str], List[str]]):
+        with torch.no_grad():
+            embedded_samples: List[Dict[str, Any]] = self.embed_data(*samples)
+            mem = SeqCollector()
+            keys = ('x', 'y', 'x_id', 'y_id', 'y_tree')
+            for sample in embedded_samples:
+                y_id_str = [str(x) for x in sample['y_id']]
+                y_emb = sample['y_emb']
+                prob_net = self._models[0]
+                y_term_prob = prob_net(y_emb)  # (num_toks, 1)
+                _, tree = TreeSampler(bounded_terms).sample(y_id_str, y_term_prob.squeeze(-1))
+                mem(x=sample['x'], y=sample['y'],
+                    x_id=sample['x_id'], y_id=sample['y_id'],
+                    y_tree=tree)
+            output = dict((k, mem[k]) for k in keys)
+            return output
 
 
 def get_tree_emb(term_embs: torch.Tensor, tree: Tree):
@@ -226,68 +304,96 @@ def get_tree_emb(term_embs: torch.Tensor, tree: Tree):
     return tgt_emb  # (num_nodes, dim)
 
 
-def sample_a_tree(terms, probs,
-                  span_begin: int = None,
-                  span_end: int = None,) -> Tuple[torch.Tensor, Tree]:
-    """
-    :param probs:  (num_toks,)
-    :param terms:   [str(id) of terms] len=num_toks
-    :param span_begin: start of span for a tree
-    :param span_end: end of span for a tree, not included
-    :return: tuple of (tree_logp, tree)
-    """
-    nt_label = 'NT'
-    span_begin = 0 if span_begin is None else span_begin
-    span_end = len(terms) if span_end is None else span_end
-
-    length = span_end - span_begin
-    if length <= 3:
-        return math.log(2), Tree(nt_label, children=[ # noqa
-            Tree(terms[span_begin + i], is_terminal=True) for i in range(length)
-        ])
-
-    span_p = probs[span_begin:span_end]
-    be_terms = span_p.bernoulli()   # (span_length,)
+def bernoulli_samples(span_prob) -> torch.Tensor:
+    be_terms = span_prob.bernoulli()   # (span_length,)
     i = 0
     while be_terms.sum() < 1:
         del be_terms
         i += .1
-        be_terms = (span_p + i).clamp(max=.99).bernoulli()  # (span_length,)
+        be_terms = (span_prob + i).clamp(max=.99).bernoulli()  # (span_length,)
+    return be_terms
 
-    level_logp = (be_terms * (span_p + 1).log()
-                  + (1 - be_terms) * (2 - span_p).log()).sum()
 
-    terminal_locs = torch.argwhere(be_terms).squeeze(-1) + span_begin  # (num_immediate_children,)
-    del be_terms
+def bounded_terms(span_prob: torch.Tensor, num_max_terms: int = 7) -> torch.Tensor:
+    """
+    :param span_prob: (span_length,), values are [0, 1)
+    :param num_max_terms:
+    :return: (span_length,)
+    """
+    k = min(num_max_terms, len(span_prob) // 3)
+    _, locations = torch.topk(span_prob, k, sorted=False)   # (k,)
+    output = torch.zeros_like(span_prob)
+    output[locations] = 1
+    return output
 
-    sub_logp = []
-    children = []
-    for i, loc in enumerate(terminal_locs):     # iterate over only terminals
-        # consider possible subtrees / nonterminals before the current terminal
-        if i == 0 and loc > span_begin:
-            # there's a subtree before the first terminal
-            subtree_logp, subtree = sample_a_tree(terms, probs, span_begin, loc)
-            sub_logp.append(subtree_logp)
-            children.append(subtree)
 
-        elif loc > terminal_locs[i - 1] + 1:
-            # a subtree between last and this terminals
-            subtree_logp, subtree = sample_a_tree(terms, probs, terminal_locs[i - 1] + 1, loc)
-            sub_logp.append(subtree_logp)
-            children.append(subtree)
+class TreeSampler:
+    def __init__(self, fn_get_terms: Callable = None, nt_label: str = 'NT'):
+        """
+        :param fn_get_terms: a callable function that reads span probabilities,
+                    and produce an 1D binary vector of the span length,
+                    indicating the token is a direct terminal (flag=1)
+                    or contained in the subtree (flag=0)
+        :param nt_label:
+        """
+        self.get_terms = fn_get_terms if fn_get_terms is not None else bernoulli_samples
+        self.nt_label = nt_label
 
-        tree = Tree(terms[loc], is_terminal=True)
-        children.append(tree)
+    def sample(self, terms, probs,
+               span_begin: int = None,
+               span_end: int = None,
+               ) -> Tuple[torch.Tensor, Tree]:
+        """
+        :param probs:  (num_toks,)
+        :param terms:   [str(id) of terms] len=num_toks
+        :param span_begin: start of span for a tree
+        :param span_end: end of span for a tree, not included
+        :return: tuple of (tree_logp, tree)
+        """
+        span_begin = 0 if span_begin is None else span_begin
+        span_end = len(terms) if span_end is None else span_end
 
-        # a subtree after the last terminal, add it before ending the loop
-        if i == len(terminal_locs) - 1 and loc < span_end - 1:
-            subtree_logp, subtree = sample_a_tree(terms, probs, loc + 1, span_end)
-            sub_logp.append(subtree_logp)
-            children.append(subtree)
+        length = span_end - span_begin
+        if length <= 3:
+            return math.log(2), Tree(self.nt_label, children=[ # noqa
+                Tree(terms[span_begin + i], is_terminal=True) for i in range(length)
+            ])
 
-    tree_logp = level_logp + sum(sub_logp, 0)
-    tree = Tree(nt_label, children=children)
-    return tree_logp, tree
+        span_p = probs[span_begin:span_end]
+        be_terms = self.get_terms(span_p)
+
+        level_logp = (be_terms * (span_p + 1).log() + (1 - be_terms) * (2 - span_p).log()).sum()
+        terminal_locs = torch.argwhere(be_terms).squeeze(-1) + span_begin  # (num_immediate_children,)
+        del be_terms
+
+        sub_logp = []
+        children = []
+        for i, loc in enumerate(terminal_locs):     # iterate over only terminals
+            # consider possible subtrees / nonterminals before the current terminal
+            if i == 0 and loc > span_begin:
+                # there's a subtree before the first terminal
+                subtree_logp, subtree = self.sample(terms, probs, span_begin, loc)
+                sub_logp.append(subtree_logp)
+                children.append(subtree)
+
+            elif loc > terminal_locs[i - 1] + 1:
+                # a subtree between last and this terminals
+                subtree_logp, subtree = self.sample(terms, probs, terminal_locs[i - 1] + 1, loc)
+                sub_logp.append(subtree_logp)
+                children.append(subtree)
+
+            tree = Tree(terms[loc], is_terminal=True)
+            children.append(tree)
+
+            # a subtree after the last terminal, add it before ending the loop
+            if i == len(terminal_locs) - 1 and loc < span_end - 1:
+                subtree_logp, subtree = self.sample(terms, probs, loc + 1, span_end)
+                sub_logp.append(subtree_logp)
+                children.append(subtree)
+
+        tree_logp = level_logp + sum(sub_logp, 0)
+        tree = Tree(self.nt_label, children=children)
+        return tree_logp, tree
 
 
 @Registry.hparamset()
